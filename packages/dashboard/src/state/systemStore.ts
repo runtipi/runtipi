b67e41995a61cfee4d8859897e2489faa1c8cebe
Nicolas Meienberger <github@thisprops.com>
2022-11-08 19:09:00 +0100

    feat: open apps from same domain in the dashboard

--- packages/dashboard/src/state/systemStore.ts
+++ packages/dashboard/src/state/systemStore.ts
@@ -8,22 +8,14 @@ export enum SystemStatus {
 
 type Store = {
   baseUrl: string;
-  internalIp: string;
-  domain: string;
   status: SystemStatus;
-  setDomain: (domain?: string) => void;
   setBaseUrl: (url: string) => void;
-  setInternalIp: (ip: string) => void;
   setStatus: (status: SystemStatus) => void;
 };
 
 export const useSystemStore = create<Store>((set) => ({
   baseUrl: '',
-  internalIp: '',
-  domain: '',
   status: SystemStatus.RUNNING,
-  setDomain: (domain?: string) => set((state) => ({ ...state, domain: domain || '' })),
   setBaseUrl: (url: string) => set((state) => ({ ...state, baseUrl: url })),
-  setInternalIp: (ip: string) => set((state) => ({ ...state, internalIp: ip })),
   setStatus: (status: SystemStatus) => set((state) => ({ ...state, status })),
 }));
