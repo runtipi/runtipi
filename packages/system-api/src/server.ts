36a6483ff78cb9552f2206ca45291b64837a9c62
Nicolas Meienberger <github@thisprops.com>
2023-02-02 18:22:18 +0100

    chore: cleanup system-api from now un-used files

--- packages/system-api/src/server.ts
+++ packages/system-api/src/server.ts
@@ -1,35 +1,16 @@
 import 'reflect-metadata';
 import express from 'express';
 import { ApolloServerPluginLandingPageGraphQLPlayground as Playground } from 'apollo-server-core';
-import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import { ZodError } from 'zod';
 import cors, { CorsOptions } from 'cors';
-import { createSchema } from './schema';
 import { ApolloLogs } from './config/logger/apollo.logger';
 import logger from './config/logger/logger';
 import getSessionMiddleware from './core/middlewares/sessionMiddleware';
-import { MyContext } from './types';
 import { __prod__ } from './config/constants/constants';
-import datasource from './config/datasource';
-import appsService from './modules/apps/apps.service';
-import { runUpdates } from './core/updates/run';
 import startJobs from './core/jobs/jobs';
-import { applyJsonConfig, getConfig, setConfig } from './core/config/TipiConfig';
-import { eventDispatcher, EventTypes } from './core/config/EventDispatcher';
-
-const applyCustomConfig = () => {
-  try {
-    applyJsonConfig();
-  } catch (e) {
-    logger.error('Error applying settings.json config');
-    if (e instanceof ZodError) {
-      Object.keys(e.flatten().fieldErrors).forEach((key) => {
-        logger.error(`Error in field ${key}`);
-      });
-    }
-  }
-};
+import { applyJsonConfig, getConfig } from './core/config/TipiConfig';
+import { eventDispatcher } from './core/config/EventDispatcher';
 
 const corsOptions: CorsOptions = {
   credentials: false,
@@ -41,7 +22,6 @@ const corsOptions: CorsOptions = {
 const main = async () => {
   try {
     eventDispatcher.clear();
-    applyCustomConfig();
 
     const app = express();
     const port = 3001;
@@ -50,37 +30,12 @@ const main = async () => {
     app.use(express.static(`${getConfig().rootFolder}/repos/${getConfig().appsRepoId}`));
     app.use(getSessionMiddleware);
 
-    await datasource.initialize();
-
-    const schema = await createSchema();
     const httpServer = createServer(app);
-    const plugins = [ApolloLogs];
-
-    if (!__prod__) {
-      plugins.push(Playground());
-    }
-
-    const apolloServer = new ApolloServer({
-      schema,
-      context: ({ req, res }): MyContext => ({ req, res }),
-      plugins,
-      cache: 'bounded',
-    });
-
-    await apolloServer.start();
-    apolloServer.applyMiddleware({ app });
-
-    await runUpdates();
 
     httpServer.listen(port, async () => {
-      await eventDispatcher.dispatchEventAsync(EventTypes.CLONE_REPO, [getConfig().appsRepoUrl]);
-      await eventDispatcher.dispatchEventAsync(EventTypes.UPDATE_REPO, [getConfig().appsRepoUrl]);
-
       startJobs();
-      setConfig('status', 'RUNNING');
 
       // Start apps
-      appsService.startAllApps();
       logger.info(`Server running on port ${port} 🚀 Production => ${__prod__}`);
       logger.info(`Config: ${JSON.stringify(getConfig(), null, 2)}`);
     });
