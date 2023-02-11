66afbf95a0e707feb8c7117d7a928444b909ee0a
Nicolas Meienberger <github@thisprops.com>
2023-02-12 00:01:00 +0100

    refactor(server): move auth and system services to class

--- packages/dashboard/src/server/services/auth/auth.service.ts
+++ packages/dashboard/src/server/services/auth/auth.service.ts
@@ -1,10 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import * as argon2 from 'argon2';
 import { v4 } from 'uuid';
 import jwt from 'jsonwebtoken';
 import validator from 'validator';
 import { getConfig } from '../../core/TipiConfig';
 import TipiCache from '../../core/TipiCache';
-import { prisma } from '../../db/client';
 
 type UsernamePasswordInput = {
   username: string;
@@ -15,141 +15,138 @@ type TokenResponse = {
   token: string;
 };
 
-/**
- * Authenticate user with given username and password
- *
- * @param {UsernamePasswordInput} input - An object containing the user's username and password
- * @return {Promise<{token:string}>} - A promise that resolves to an object containing the JWT token
- */
-const login = async (input: UsernamePasswordInput) => {
-  const { password, username } = input;
+export class AuthServiceClass {
+  private prisma;
 
-  const user = await prisma.user.findUnique({ where: { username: username.trim().toLowerCase() } });
-
-  if (!user) {
-    throw new Error('User not found');
+  constructor(p: PrismaClient) {
+    this.prisma = p;
   }
 
-  const isPasswordValid = await argon2.verify(user.password, password);
+  /**
+   * Authenticate user with given username and password
+   *
+   * @param {UsernamePasswordInput} input - An object containing the user's username and password
+   * @returns {Promise<{token:string}>} - A promise that resolves to an object containing the JWT token
+   */
+  public login = async (input: UsernamePasswordInput) => {
+    const { password, username } = input;
 
-  if (!isPasswordValid) {
-    throw new Error('Wrong password');
-  }
+    const user = await this.prisma.user.findUnique({ where: { username: username.trim().toLowerCase() } });
 
-  const session = v4();
-  const token = jwt.sign({ id: user.id, session }, getConfig().jwtSecret, { expiresIn: '7d' });
+    if (!user) {
+      throw new Error('User not found');
+    }
 
-  await TipiCache.set(session, user.id.toString());
+    const isPasswordValid = await argon2.verify(user.password, password);
 
-  return { token };
-};
+    if (!isPasswordValid) {
+      throw new Error('Wrong password');
+    }
 
-/**
- * Creates a new user with the provided email and password and returns a session token
- *
- * @param {UsernamePasswordInput} input - An object containing the email and password fields
- * @returns {Promise<{token: string}>} - An object containing the session token
- * @throws {Error} - If the email or password is missing, the email is invalid or the user already exists
- */
-const register = async (input: UsernamePasswordInput) => {
-  const { password, username } = input;
-  const email = username.trim().toLowerCase();
+    const session = v4();
+    const token = jwt.sign({ id: user.id, session }, getConfig().jwtSecret, { expiresIn: '7d' });
 
-  if (!username || !password) {
-    throw new Error('Missing email or password');
-  }
+    await TipiCache.set(session, user.id.toString());
 
-  if (username.length < 3 || !validator.isEmail(email)) {
-    throw new Error('Invalid username');
-  }
+    return { token };
+  };
 
-  const user = await prisma.user.findUnique({ where: { username: email } });
+  /**
+   * Creates a new user with the provided email and password and returns a session token
+   *
+   * @param {UsernamePasswordInput} input - An object containing the email and password fields
+   * @returns {Promise<{token: string}>} - An object containing the session token
+   * @throws {Error} - If the email or password is missing, the email is invalid or the user already exists
+   */
+  public register = async (input: UsernamePasswordInput) => {
+    const { password, username } = input;
+    const email = username.trim().toLowerCase();
 
-  if (user) {
-    throw new Error('User already exists');
-  }
+    if (!username || !password) {
+      throw new Error('Missing email or password');
+    }
 
-  const hash = await argon2.hash(password);
-  const newUser = await prisma.user.create({ data: { username: email, password: hash } });
+    if (username.length < 3 || !validator.isEmail(email)) {
+      throw new Error('Invalid username');
+    }
 
-  const session = v4();
-  const token = jwt.sign({ id: newUser.id, session }, getConfig().jwtSecret, { expiresIn: '1d' });
+    const user = await this.prisma.user.findUnique({ where: { username: email } });
 
-  await TipiCache.set(session, newUser.id.toString());
+    if (user) {
+      throw new Error('User already exists');
+    }
 
-  return { token };
-};
+    const hash = await argon2.hash(password);
+    const newUser = await this.prisma.user.create({ data: { username: email, password: hash } });
 
-/**
- * Retrieves the user with the provided ID
- *
- * @param {number|undefined} userId - The user ID to retrieve
- * @returns {Promise<{id: number, username: string} | null>} - An object containing the user's id and email, or null if the user is not found
- */
-const me = async (userId: number | undefined) => {
-  if (!userId) return null;
+    const session = v4();
+    const token = jwt.sign({ id: newUser.id, session }, getConfig().jwtSecret, { expiresIn: '1d' });
 
-  const user = await prisma.user.findUnique({ where: { id: Number(userId) }, select: { id: true, username: true } });
+    await TipiCache.set(session, newUser.id.toString());
 
-  if (!user) return null;
+    return { token };
+  };
 
-  return user;
-};
+  /**
+   * Retrieves the user with the provided ID
+   *
+   * @param {number|undefined} userId - The user ID to retrieve
+   * @returns {Promise<{id: number, username: string} | null>} - An object containing the user's id and email, or null if the user is not found
+   */
+  public me = async (userId: number | undefined) => {
+    if (!userId) return null;
 
-/**
- * Logs out the current user by removing the session token
- *
- * @param {string} [session] - The session token to log out
- * @returns {Promise<boolean>} - Returns true if the session token is removed successfully
- */
-const logout = async (session?: string): Promise<boolean> => {
-  if (session) {
-    await TipiCache.del(session);
-  }
+    const user = await this.prisma.user.findUnique({ where: { id: Number(userId) }, select: { id: true, username: true } });
 
-  return true;
-};
+    if (!user) return null;
 
-/**
- * Refreshes a user's session token
- *
- * @param {string} [session] - The current session token
- * @returns {Promise<{token: string} | null>} - An object containing the new session token, or null if the session is invalid
- */
-const refreshToken = async (session?: string): Promise<TokenResponse | null> => {
-  if (!session) return null;
+    return user;
+  };
 
-  const userId = await TipiCache.get(session);
-  if (!userId) return null;
+  /**
+   * Logs out the current user by removing the session token
+   *
+   * @param {string} [session] - The session token to log out
+   * @returns {Promise<boolean>} - Returns true if the session token is removed successfully
+   */
+  public static logout = async (session?: string): Promise<boolean> => {
+    if (session) {
+      await TipiCache.del(session);
+    }
 
-  // Expire token in 6 seconds
-  await TipiCache.set(session, userId, 6);
+    return true;
+  };
 
-  const newSession = v4();
-  const token = jwt.sign({ id: userId, session: newSession }, getConfig().jwtSecret, { expiresIn: '1d' });
-  await TipiCache.set(newSession, userId);
+  /**
+   * Refreshes a user's session token
+   *
+   * @param {string} [session] - The current session token
+   * @returns {Promise<{token: string} | null>} - An object containing the new session token, or null if the session is invalid
+   */
+  public static refreshToken = async (session?: string): Promise<TokenResponse | null> => {
+    if (!session) return null;
 
-  return { token };
-};
+    const userId = await TipiCache.get(session);
+    if (!userId) return null;
 
-/**
- * Check if the system is configured and has at least one user
- *
- * @returns {Promise<boolean>} - A boolean indicating if the system is configured or not
- */
-const isConfigured = async (): Promise<boolean> => {
-  const count = await prisma.user.count();
+    // Expire token in 6 seconds
+    await TipiCache.set(session, userId, 6);
 
-  return count > 0;
-};
+    const newSession = v4();
+    const token = jwt.sign({ id: userId, session: newSession }, getConfig().jwtSecret, { expiresIn: '1d' });
+    await TipiCache.set(newSession, userId);
 
-const AuthService = {
-  login,
-  register,
-  me,
-  logout,
-  refreshToken,
-  isConfigured,
-};
+    return { token };
+  };
 
-export default AuthService;
+  /**
+   * Check if the system is configured and has at least one user
+   *
+   * @returns {Promise<boolean>} - A boolean indicating if the system is configured or not
+   */
+  public isConfigured = async (): Promise<boolean> => {
+    const count = await this.prisma.user.count();
+
+    return count > 0;
+  };
+}
--- packages/dashboard/src/server/tests/user.factory.ts
+++ packages/dashboard/src/server/tests/user.factory.ts
@@ -3,11 +3,11 @@ import { faker } from '@faker-js/faker';
 import * as argon2 from 'argon2';
 import { prisma } from '../db/client';
 
-const createUser = async (email?: string) => {
+const createUser = async (email?: string, db = prisma) => {
   const hash = await argon2.hash('password');
 
   const username = email?.toLowerCase().trim() || faker.internet.email().toLowerCase().trim();
-  const user = await prisma.user.create({ data: { username, password: hash } });
+  const user = await db.user.create({ data: { username, password: hash } });
 
   return user;
 };
