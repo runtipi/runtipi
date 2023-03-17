ba0925f5984d325455d59ce958d77688d0fa0279
Nicolas Meienberger <github@thisprops.com>
2023-03-17 18:08:47 +0100

    fix(app-store): incorrect breadcrumbs displayed in app store nested routes

--- src/pages/apps/[id].tsx
+++ src/pages/apps/[id].tsx
@@ -1,1 +1,13 @@
-export { AppDetailsPage as default } from '../../client/modules/Apps/pages/AppDetailsPage';
+import { AppDetailsPage } from '../../client/modules/Apps/pages/AppDetailsPage';
+
+const Page = AppDetailsPage;
+
+Page.getInitialProps = (ctx) => {
+  const { query } = ctx;
+
+  const appId = String(query.id);
+
+  return { appId, refSlug: 'apps', refTitle: 'Apps' };
+};
+
+export default Page;
