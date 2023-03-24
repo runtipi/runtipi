20baa4e5e1be3973f59660c019f11c9f9b88fc3a
Nicolas Meienberger <github@thisprops.com>
2023-03-24 08:44:21 +0100

    chore: add react-query dev tools and update lock file

--- public/mockServiceWorker.js
+++ public/mockServiceWorker.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (1.0.1).
+ * Mock Service Worker (1.1.0).
  * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
