a4a6e900f0b647d40ef7b673cc5a4ddff2e13aa8
Nicolas Meienberger <github@thisprops.com>
2023-03-24 08:43:51 +0100

    chore: updated outdated dependencies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function MyDocument() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href={getUrl('apple-touch-icon.png')} />
