564827a76f5ac1aef85fa6c5659832fc25c49ae2
Nicolas Meienberger <github@thisprops.com>
2023-04-11 08:31:33 +0200

    fix: tooltips mixed content in app status

--- src/client/components/AppStatus/AppStatus.tsx
+++ src/client/components/AppStatus/AppStatus.tsx
@@ -14,12 +14,8 @@ export const AppStatus: React.FC<{ status: AppTypes.AppStatus; lite?: boolean }>
 
   return (
     <>
-      {lite && (
-        <Tooltip anchorSelect=".appStatus" place="top">
-          {formattedStatus}
-        </Tooltip>
-      )}
-      <div className="appStatus d-flex align-items-center">
+      {lite && <Tooltip id={formattedStatus} anchorSelect=".appStatus" place="top" />}
+      <div data-tooltip-content={formattedStatus} data-tooltip-id={formattedStatus} className="appStatus d-flex align-items-center">
         <span className={classes} />
         {!lite && <span className={clsx(styles.text, 'ms-2 text-muted')}>{formattedStatus}</span>}
       </div>
