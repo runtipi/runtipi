6bbd950d15bf852486283c2f2516990a4e0501ff
Nicolas Meienberger <github@thisprops.com>
2023-04-25 20:16:12 +0200

    feat: create start:e2e and test:e2e scripts to launch tests easily

--- scripts/start.sh
+++ scripts/start.sh
@@ -33,105 +33,16 @@ fi
 ### --------------------------------
 ### General variables
 ### --------------------------------
-DEFAULT_TZ="Etc\/UTC"
-TZ="$(timedatectl | grep "Time zone" | awk '{print $3}' | sed 's/\//\\\//g')"
-if [[ -z "$TZ" ]]; then
-  TZ="$DEFAULT_TZ"
-fi
-NGINX_PORT=80
-NGINX_PORT_SSL=443
-DOMAIN=tipi.localhost
-SED_ROOT_FOLDER="$(echo "$ROOT_FOLDER" | sed 's/\//\\\//g')"
-DNS_IP="9.9.9.9" # Default to Quad9 DNS
-ARCHITECTURE="$(uname -m | tr '[:upper:]' '[:lower:]')"
 apps_repository="https://github.com/meienberger/runtipi-appstore"
-REPO_ID="$("${ROOT_FOLDER}"/scripts/git.sh get_hash ${apps_repository})"
-APPS_REPOSITORY_ESCAPED="$(echo ${apps_repository} | sed 's/\//\\\//g')"
-JWT_SECRET=$(derive_entropy "jwt")
-POSTGRES_PASSWORD=$(derive_entropy "postgres")
-POSTGRES_USERNAME=tipi
-POSTGRES_DBNAME=tipi
-POSTGRES_PORT=5432
-POSTGRES_HOST=tipi-db
-TIPI_VERSION=$(get_json_field "${ROOT_FOLDER}/package.json" version)
-storage_path="${ROOT_FOLDER}"
-STORAGE_PATH_ESCAPED="$(echo "${storage_path}" | sed 's/\//\\\//g')"
-REDIS_HOST=tipi-redis
-DEMO_MODE=false
 INTERNAL_IP=
 
-if [[ "$ARCHITECTURE" == "aarch64" ]] || [[ "$ARCHITECTURE" == "armv8"* ]]; then
-  ARCHITECTURE="arm64"
-elif [[ "$ARCHITECTURE" == "x86_64" ]]; then
-  ARCHITECTURE="amd64"
+
+if [[ -f "${STATE_FOLDER}/settings.json" ]]; then
+  # If listenIp is set in settings.json, use it
+  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" listenIp)" != "null" ]]; then
+    INTERNAL_IP=$(get_json_field "${STATE_FOLDER}/settings.json" listenIp)
+  fi
 fi
-
-# If none of the above conditions are met, the architecture is not supported
-if [[ "$ARCHITECTURE" != "arm64" ]] && [[ "$ARCHITECTURE" != "amd64" ]]; then
-  echo "Architecture ${ARCHITECTURE} not supported if you think this is a mistake, please open an issue on GitHub."
-  exit 1
-fi
-
-### --------------------------------
-### CLI arguments
-### --------------------------------
-while [ -n "${1-}" ]; do
-  case "$1" in
-  --rc) rc="true" ;;
-  --ci) ci="true" ;;
-  --demo) DEMO_MODE=true ;;
-  --port)
-    port="${2-}"
-
-    if [[ "${port}" =~ ^[0-9]+$ ]]; then
-      NGINX_PORT="${port}"
-    else
-      echo "--port must be a number"
-      exit 1
-    fi
-    shift
-    ;;
-  --ssl-port)
-    ssl_port="${2-}"
-
-    if [[ "${ssl_port}" =~ ^[0-9]+$ ]]; then
-      NGINX_PORT_SSL="${ssl_port}"
-    else
-      echo "--ssl-port must be a number"
-      exit 1
-    fi
-    shift
-    ;;
-  --domain)
-    domain="${2-}"
-
-    if [[ "${domain}" =~ ^[a-zA-Z0-9.-]+$ ]]; then
-      DOMAIN="${domain}"
-    else
-      echo "--domain must be a valid domain"
-      exit 1
-    fi
-    shift
-    ;;
-  --listen-ip)
-    listen_ip="${2-}"
-
-    if [[ "${listen_ip}" =~ ^[a-fA-F0-9.:]+$ ]]; then
-      INTERNAL_IP="${listen_ip}"
-    else
-      echo "--listen-ip must be a valid IP address"
-      exit 1
-    fi
-    shift
-    ;;
-  --)
-    shift # The double dash makes them parameters
-    break
-    ;;
-  *) echo "Option $1 not recognized" && exit 1 ;;
-  esac
-  shift
-done
 
 if [[ -z "${INTERNAL_IP:-}" ]]; then
   network_interface="$(ip route | grep default | awk '{print $5}' | uniq)"
@@ -175,11 +86,33 @@ if [[ -z "${INTERNAL_IP:-}" ]]; then
   fi
 fi
 
-# If port is not 80 and domain is not tipi.localhost, we exit
-if [[ "${NGINX_PORT}" != "80" ]] && [[ "${DOMAIN}" != "tipi.localhost" ]]; then
-  echo "Using a custom domain with a custom port is not supported"
-  exit 1
-fi
+env_variables_json=$(cat <<EOF
+{
+  "dns_ip": "9.9.9.9",
+  "internal_ip": "${INTERNAL_IP}",
+  "jwt_secret": "$(derive_entropy "jwt")",
+  "root_folder": "${ROOT_FOLDER}",
+  "tipi_version": "$(get_json_field "${ROOT_FOLDER}/package.json" version)",
+  "nginx_port": 80,
+  "nginx_port_ssl": 443,
+  "postgres_password": "$(derive_entropy "postgres")
+  "postgres_username": "tipi",
+  "postgres_dbname": "tipi",
+  "postgres_port": 5432,
+  "postgres_host": "tipi-db",
+  "redis_host": "tipi-redis",
+  "repo_id": "$("${ROOT_FOLDER}"/scripts/git.sh get_hash "${apps_repository}")",
+  "apps_repository": "${apps_repository}",
+  "domain": "tipi.localhost",
+  "storage_path": "${ROOT_FOLDER}",
+  "demo_mode": false,
+}
+EOF
+)
+
+echo "Generating config files..."
+write_log "Final values: \n${env_variables_json}"
+generate_env_file "${env_variables_json}"
 
 ### --------------------------------
 ### Watcher and system-info
@@ -190,117 +123,24 @@ echo "Running system-info.sh..."
 kill_watcher
 "${ROOT_FOLDER}/scripts/watcher.sh" &
 
-### --------------------------------
-### settings.json overrides
-### --------------------------------
-echo "Generating config files..."
-# Override vars with values from settings.json
-if [[ -f "${STATE_FOLDER}/settings.json" ]]; then
-
-  # If dnsIp is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" dnsIp)" != "null" ]]; then
-    DNS_IP=$(get_json_field "${STATE_FOLDER}/settings.json" dnsIp)
-  fi
-
-  # If domain is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" domain)" != "null" ]]; then
-    DOMAIN=$(get_json_field "${STATE_FOLDER}/settings.json" domain)
-  fi
-
-  # If appsRepoUrl is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" appsRepoUrl)" != "null" ]]; then
-    apps_repository=$(get_json_field "${STATE_FOLDER}/settings.json" appsRepoUrl)
-    APPS_REPOSITORY_ESCAPED="$(echo "${apps_repository}" | sed 's/\//\\\//g')"
-    REPO_ID="$("${ROOT_FOLDER}"/scripts/git.sh get_hash "${apps_repository}")"
-  fi
-
-  # If port is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" port)" != "null" ]]; then
-    NGINX_PORT=$(get_json_field "${STATE_FOLDER}/settings.json" port)
-  fi
-
-  # If sslPort is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" sslPort)" != "null" ]]; then
-    NGINX_PORT_SSL=$(get_json_field "${STATE_FOLDER}/settings.json" sslPort)
-  fi
-
-  # If listenIp is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" listenIp)" != "null" ]]; then
-    INTERNAL_IP=$(get_json_field "${STATE_FOLDER}/settings.json" listenIp)
-  fi
-
-  # If demoMode is set in settings.json, use it
-  if [[ "$(get_json_field "${STATE_FOLDER}/settings.json" demoMode)" == "true" ]]; then
-    DEMO_MODE="true"
-  fi
-
-  # If storagePath is set in settings.json, use it
-  storage_path_settings=$(get_json_field "${STATE_FOLDER}/settings.json" storagePath)
-  if [[ "${storage_path_settings}" != "null" && "${storage_path_settings}" != "" ]]; then
-    storage_path="${storage_path_settings}"
-    STORAGE_PATH_ESCAPED="$(echo "${storage_path}" | sed 's/\//\\\//g')"
-  fi
-fi
-
-new_values="DOMAIN=${DOMAIN}\nDNS_IP=${DNS_IP}\nAPPS_REPOSITORY=${APPS_REPOSITORY_ESCAPED}\nREPO_ID=${REPO_ID}\nNGINX_PORT=${NGINX_PORT}\nNGINX_PORT_SSL=${NGINX_PORT_SSL}\nINTERNAL_IP=${INTERNAL_IP}\nSTORAGE_PATH=${STORAGE_PATH_ESCAPED}\nTZ=${TZ}\nJWT_SECRET=${JWT_SECRET}\nROOT_FOLDER=${SED_ROOT_FOLDER}\nTIPI_VERSION=${TIPI_VERSION}\nARCHITECTURE=${ARCHITECTURE}"
-write_log "Final values: \n${new_values}"
-
-### --------------------------------
-### env file generation
-### --------------------------------
-ENV_FILE=$(mktemp)
-[[ -f "${ROOT_FOLDER}/.env" ]] && rm -f "${ROOT_FOLDER}/.env"
-[[ -f "$ROOT_FOLDER/templates/env-sample" ]] && cp "$ROOT_FOLDER/templates/env-sample" "$ENV_FILE"
-
-
-# Function below is modified from Umbrel
-# Required Notice: Copyright
-# Umbrel (https://umbrel.com)
-for template in ${ENV_FILE}; do
-  sed -i "s/<dns_ip>/${DNS_IP}/g" "${template}"
-  sed -i "s/<internal_ip>/${INTERNAL_IP}/g" "${template}"
-  sed -i "s/<tz>/${TZ}/g" "${template}"
-  sed -i "s/<jwt_secret>/${JWT_SECRET}/g" "${template}"
-  sed -i "s/<root_folder>/${SED_ROOT_FOLDER}/g" "${template}"
-  sed -i "s/<tipi_version>/${TIPI_VERSION}/g" "${template}"
-  sed -i "s/<architecture>/${ARCHITECTURE}/g" "${template}"
-  sed -i "s/<nginx_port>/${NGINX_PORT}/g" "${template}"
-  sed -i "s/<nginx_port_ssl>/${NGINX_PORT_SSL}/g" "${template}"
-  sed -i "s/<postgres_password>/${POSTGRES_PASSWORD}/g" "${template}"
-  sed -i "s/<postgres_username>/${POSTGRES_USERNAME}/g" "${template}"
-  sed -i "s/<postgres_dbname>/${POSTGRES_DBNAME}/g" "${template}"
-  sed -i "s/<postgres_port>/${POSTGRES_PORT}/g" "${template}"
-  sed -i "s/<postgres_host>/${POSTGRES_HOST}/g" "${template}"
-  sed -i "s/<apps_repo_id>/${REPO_ID}/g" "${template}"
-  sed -i "s/<apps_repo_url>/${APPS_REPOSITORY_ESCAPED}/g" "${template}"
-  sed -i "s/<domain>/${DOMAIN}/g" "${template}"
-  sed -i "s/<storage_path>/${STORAGE_PATH_ESCAPED}/g" "${template}"
-  sed -i "s/<redis_host>/${REDIS_HOST}/g" "${template}"
-  sed -i "s/<demo_mode>/${DEMO_MODE}/g" "${template}"
-done
-
-mv -f "$ENV_FILE" "$ROOT_FOLDER/.env"
 
 ### --------------------------------
 ### Start the project
 ### --------------------------------
-if [[ ! "${ci-false}" == "true" ]]; then
-
-  if [[ "${rc-false}" == "true" ]]; then
-    docker compose -f docker-compose.rc.yml --env-file "${ROOT_FOLDER}/.env" pull
-    # Run docker compose
-    docker compose -f docker-compose.rc.yml --env-file "${ROOT_FOLDER}/.env" up --detach --remove-orphans --build || {
-      echo "Failed to start containers"
-      exit 1
-    }
-  else
-    docker compose --env-file "${ROOT_FOLDER}/.env" pull
-    # Run docker compose
-    docker compose --env-file "${ROOT_FOLDER}/.env" up --detach --remove-orphans --build || {
-      echo "Failed to start containers"
-      exit 1
-    }
-  fi
+if [[ "${rc-false}" == "true" ]]; then
+  docker compose -f docker-compose.rc.yml --env-file "${ROOT_FOLDER}/.env" pull
+  # Run docker compose
+  docker compose -f docker-compose.rc.yml --env-file "${ROOT_FOLDER}/.env" up --detach --remove-orphans --build || {
+    echo "Failed to start containers"
+    exit 1
+  }
+else
+  docker compose --env-file "${ROOT_FOLDER}/.env" pull
+  # Run docker compose
+  docker compose --env-file "${ROOT_FOLDER}/.env" up --detach --remove-orphans --build || {
+    echo "Failed to start containers"
+    exit 1
+  }
 fi
 
 echo "Tipi is now running"
@@ -328,11 +168,6 @@ cat <<"EOF"
            ()`     
 EOF
 
-port_display=""
-if [[ $NGINX_PORT != "80" ]]; then
-  port_display=":${NGINX_PORT}"
-fi
-
 echo ""
-echo "Visit http://${INTERNAL_IP}${port_display}/ to view the dashboard"
+echo "Visit http://${INTERNAL_IP}/ to view the dashboard"
 echo ""
