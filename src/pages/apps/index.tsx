f94cbe7bd661bf975b3957cb1fdad22570b938fd
Nicolas Meienberger <github@thisprops.com>
2023-05-03 22:51:46 +0200

    feat(client): implement server-side redirect to authenticated pages

--- src/pages/apps/index.tsx
+++ src/pages/apps/index.tsx
@@ -1,1 +1,13 @@
+import { getAuthedPageProps } from '@/utils/page-helpers';
+import { GetServerSideProps } from 'next';
+
 export { AppsPage as default } from '../../client/modules/Apps/pages/AppsPage';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const authedProps = await getAuthedPageProps(ctx);
+
+  return {
+    ...authedProps,
+    props: {},
+  };
+};
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,1 +1,9 @@
+import { GetServerSideProps } from 'next';
+
 export { RegisterPage as default } from '../client/modules/Auth/pages/RegisterPage';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  };
+};
