1fca513c755a5f14bf67818eaa87301f3a4fd693
Nicolas Meienberger <github@thisprops.com>
2023-05-06 13:19:07 +0200

    feat: setup next-intl with initial page props

--- src/pages/app-store/index.tsx
+++ src/pages/app-store/index.tsx
@@ -1,13 +1,14 @@
-import { getAuthedPageProps } from '@/utils/page-helpers';
+import { getAuthedPageProps, getMessagesPageProps } from '@/utils/page-helpers';
+import merge from 'lodash.merge';
 import { GetServerSideProps } from 'next';
 
 export { AppStorePage as default } from '../../client/modules/AppStore/pages/AppStorePage';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const authedProps = await getAuthedPageProps(ctx);
+  const messagesProps = await getMessagesPageProps(ctx);
 
-  return {
-    ...authedProps,
+  return merge(authedProps, messagesProps, {
     props: {},
-  };
+  });
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import { getAuthedPageProps } from '@/utils/page-helpers';
+import { getAuthedPageProps, getMessagesPageProps } from '@/utils/page-helpers';
+import merge from 'lodash.merge';
 import { GetServerSideProps } from 'next';
 
 export { DashboardPage as default } from '../client/modules/Dashboard/pages/DashboardPage';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const authedProps = await getAuthedPageProps(ctx);
+  const messagesProps = await getMessagesPageProps(ctx);
 
-  return {
-    ...authedProps,
+  return merge(authedProps, messagesProps, {
     props: {},
-  };
+  });
 };
