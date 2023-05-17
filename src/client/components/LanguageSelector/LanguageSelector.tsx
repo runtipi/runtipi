1de2d13d2652c4d1afbf32648828a2229647f30a
Nicolas Meienberger <github@thisprops.com>
2023-05-17 19:00:07 +0200

    fix: docker build failing due to missing external dependencies in esbuild.js

--- src/client/components/LanguageSelector/LanguageSelector.tsx
+++ src/client/components/LanguageSelector/LanguageSelector.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { useLocale } from '@/client/hooks/useLocale';
 import { LOCALE_OPTIONS, Locale } from '@/shared/internationalization/locales';
+import { useTranslations } from 'next-intl';
+import { IconExternalLink } from '@tabler/icons-react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/Select';
 
-export const LanguageSelector = () => {
+type IProps = {
+  showLabel?: boolean;
+};
+
+export const LanguageSelector = (props: IProps) => {
+  const { showLabel = false } = props;
+  const t = useTranslations('settings.settings');
   const { locale, changeLocale } = useLocale();
 
   const onChange = (value: Locale) => {
@@ -12,7 +20,20 @@ export const LanguageSelector = () => {
 
   return (
     <Select value={locale} defaultValue={locale} onValueChange={onChange}>
-      <SelectTrigger className="mb-3" label="">
+      <SelectTrigger
+        className="mb-3"
+        label={
+          showLabel && (
+            <span>
+              {t('language')}&nbsp;
+              <a href="https://crowdin.com/project/runtipi/invite?h=ae594e86cd807bc075310cab20a4aa921693663" target="_blank" rel="noreferrer">
+                {t('help-translate')}
+                <IconExternalLink className="ms-1 mb-1" size={16} />
+              </a>
+            </span>
+          )
+        }
+      >
         <SelectValue placeholder="test" />
       </SelectTrigger>
       <SelectContent>
--- src/client/modules/Settings/pages/SettingsPage/SettingsPage.tsx
+++ src/client/modules/Settings/pages/SettingsPage/SettingsPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/router';
 import { Layout } from '../../../../components/Layout';
 import { GeneralActions } from '../../containers/GeneralActions';
 import { SettingsContainer } from '../../containers/SettingsContainer';
@@ -9,14 +10,27 @@ import { SecurityContainer } from '../../containers/SecurityContainer';
 
 export const SettingsPage: NextPage = () => {
   const t = useTranslations('settings');
+  const router = useRouter();
+  const { tab } = router.query;
+
+  const handleTabChange = (newTab: string) => {
+    router.push(`/settings?tab=${newTab}`);
+  };
+
   return (
     <Layout title={t('title')}>
       <div className="card d-flex">
-        <Tabs defaultValue="actions">
+        <Tabs defaultValue={(tab as string) || 'actions'}>
           <TabsList>
-            <TabsTrigger value="actions">{t('actions.tab-title')}</TabsTrigger>
-            <TabsTrigger value="settings">{t('settings.tab-title')}</TabsTrigger>
-            <TabsTrigger value="security">{t('security.tab-title')}</TabsTrigger>
+            <TabsTrigger onClick={() => handleTabChange('actions')} value="actions">
+              {t('actions.tab-title')}
+            </TabsTrigger>
+            <TabsTrigger onClick={() => handleTabChange('settings')} value="settings">
+              {t('settings.tab-title')}
+            </TabsTrigger>
+            <TabsTrigger onClick={() => handleTabChange('security')} value="security">
+              {t('security.tab-title')}
+            </TabsTrigger>
           </TabsList>
           <TabsContent value="actions">
             <GeneralActions />
