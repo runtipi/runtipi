a64c0d20a89ec97e5222504f4a43988d390c0114
Nicolas Meienberger <github@thisprops.com>
2023-06-23 08:00:58 +0200

    test: install vite and create base config for server testing

---
+++ vitest.workspace.ts
@@ -0,0 +1,16 @@
+import { defineWorkspace } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineWorkspace([
+  {
+    plugins: [tsconfigPaths()],
+    test: {
+      globals: true,
+      name: 'server',
+      root: './src/server',
+      environment: 'node',
+      include: ['./services/auth/auth.service.test.ts'],
+      setupFiles: ['./tests/vite.setup.ts'],
+    },
+  },
+]);
