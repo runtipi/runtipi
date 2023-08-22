4ea9d5ec65fe92a7a78bed680a3489a4852aa85e
Nicolas Meienberger <github@thisprops.com>
2023-08-22 07:48:59 +0200

    chore: fix permission issues with running watcher sudoless

--- packages/cli/src/executors/system/system.executors.ts
+++ packages/cli/src/executors/system/system.executors.ts
@@ -147,7 +147,7 @@ export class SystemExecutors {
       const out = fs.openSync('./logs/watcher.log', 'a');
       const err = fs.openSync('./logs/watcher.log', 'a');
 
-      const subprocess = spawn('./runtipi-cli', [process.argv[1] as string, 'watch'], { cwd: this.rootFolder, detached: true, stdio: ['ignore', out, err] });
+      const subprocess = spawn('sudo ./runtipi-cli', [process.argv[1] as string, 'watch'], { cwd: this.rootFolder, detached: true, stdio: ['ignore', out, err] });
       subprocess.unref();
 
       spinner.done('Watcher started');
