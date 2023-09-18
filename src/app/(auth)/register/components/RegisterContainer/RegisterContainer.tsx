e3dc882261a33ff5a1eed24c4ce1692d64eda993
Nicolas Meienberger <github@thisprops.com>
2023-09-18 11:05:08 -0700

    feat: move register flow to RSC

---
+++ src/app/(auth)/register/components/RegisterContainer/RegisterContainer.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React from 'react';
+import { useAction } from 'next-safe-action/hook';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { registerAction } from '@/actions/register/register-action';
+import { RegisterForm } from '../RegisterForm';
+
+export const RegisterContainer: React.FC = () => {
+  const router = useRouter();
+
+  const registerMutation = useAction(registerAction, {
+    onSuccess: (data) => {
+      if (!data.success) {
+        toast.error(data.failure.reason);
+      } else {
+        router.push('/dashboard');
+      }
+    },
+  });
+
+  return <RegisterForm onSubmit={({ email, password }) => registerMutation.execute({ username: email, password })} loading={registerMutation.isExecuting} />;
+};
