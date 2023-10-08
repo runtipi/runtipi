8d690e408a57588090c991537a77245919497481
Nicolas Meienberger <github@thisprops.com>
2023-10-08 15:27:07 +0200

    chore: remove _app, _document and middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 
+import { cookies } from 'next/headers';
 import { Inter } from 'next/font/google';
 import merge from 'lodash.merge';
 import { NextIntlClientProvider } from 'next-intl';
@@ -8,7 +9,9 @@ import { NextIntlClientProvider } from 'next-intl';
 import './global.css';
 import clsx from 'clsx';
 import { Toaster } from 'react-hot-toast';
+import Head from 'next/head';
 import { getCurrentLocale } from '../utils/getCurrentLocale';
+import { ClientProviders } from './components/ClientProviders';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -27,13 +30,26 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const messages = (await import(`../client/messages/${locale}.json`)).default;
   const mergedMessages = merge(englishMessages, messages);
 
+  const theme = cookies().get('theme');
+
   return (
     <html lang={locale} className={clsx(inter.className, 'border-top-wide border-primary')}>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <NextIntlClientProvider locale={locale} messages={mergedMessages}>
-        <body>
-          {children}
-          <Toaster />
-        </body>
+        <ClientProviders initialTheme={theme?.value} cookies={cookies().getAll()}>
+          <body data-bs-theme={theme?.value}>
+            {children}
+            <Toaster />
+          </body>
+        </ClientProviders>
       </NextIntlClientProvider>
     </html>
   );
