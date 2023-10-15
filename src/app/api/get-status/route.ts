cb4207b619364cf67ac64f74d7f30de00bf27cbb
Nicolas Meienberger <github@thisprops.com>
2023-10-15 14:27:09 +0200

    fix(get-status): return error in case of cache retrieval failure

--- src/app/api/get-status/route.ts
+++ src/app/api/get-status/route.ts
@@ -1,4 +1,5 @@
 import { TipiCache } from '@/server/core/TipiCache';
+import { getConfig } from '@/server/core/TipiConfig';
 
 export async function GET() {
   try {
@@ -8,6 +9,6 @@ export async function GET() {
 
     return Response.json({ success: true, status: status || 'RUNNING' });
   } catch (error) {
-    return Response.json({ success: false, status: 'RUNNING' });
+    return Response.json({ success: false, status: 'ERROR', error, __debug_config: getConfig() });
   }
 }
