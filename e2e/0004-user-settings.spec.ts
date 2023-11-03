1296c6c3ce4b8944914197fdfab7314ed94e3348
Nicolas Meienberger <github@thisprops.com>
2023-11-03 21:00:25 +0100

    test(e2e): add suite for changing email

--- e2e/0004-user-settings.spec.ts
+++ e2e/0004-user-settings.spec.ts
@@ -2,8 +2,10 @@ import { test, expect } from '@playwright/test';
 import { loginUser } from './fixtures/fixtures';
 import { clearDatabase } from './helpers/db';
 import { testUser } from './helpers/constants';
+import { setSettings } from './helpers/settings';
 
 test.beforeEach(async ({ page }) => {
+  await setSettings({});
   await clearDatabase();
   await loginUser(page);
 
@@ -27,6 +29,52 @@ test('user can change their password', async ({ page }) => {
   // Login with new password
   await page.getByPlaceholder('you@example.com').fill(testUser.email);
   await page.getByPlaceholder('Your password').fill('password2');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+});
+
+test('user can change their email', async ({ page }) => {
+  // Change email
+  const newEmail = 'tester2@test.com';
+
+  await page.getByRole('tab', { name: 'Security' }).click();
+  await page.getByRole('button', { name: 'Change username' }).click();
+  await page.getByPlaceholder('New username').click();
+  await page.getByPlaceholder('New username').fill(newEmail);
+
+  // Wrong password
+  await page.getByPlaceholder('Password', { exact: true }).click();
+  await page.getByPlaceholder('Password', { exact: true }).fill('incorrect');
+
+  await page.getByRole('button', { name: 'Change username' }).click();
+
+  await expect(page.getByText('Invalid password')).toBeVisible();
+
+  // Wrong email
+  await page.getByPlaceholder('Password', { exact: true }).click();
+  await page.getByPlaceholder('Password', { exact: true }).fill(testUser.password);
+  await page.getByPlaceholder('New username').click();
+  await page.getByPlaceholder('New username').fill('incorrect');
+
+  await page.getByRole('button', { name: 'Change username' }).click();
+
+  await expect(page.getByText('Must be a valid email address')).toBeVisible();
+
+  // Correct email and password
+  await page.getByPlaceholder('New username').click();
+  await page.getByPlaceholder('New username').fill(newEmail);
+
+  await page.getByRole('button', { name: 'Change username' }).click();
+
+  await expect(page.getByText('Username changed successfully')).toBeVisible();
+
+  // Login with new email
+  await page.getByPlaceholder('you@example.com').click();
+  await page.getByPlaceholder('you@example.com').fill(newEmail);
+  await page.getByPlaceholder('Your password').click();
+  await page.getByPlaceholder('Your password').fill(testUser.password);
+
   await page.getByRole('button', { name: 'Login' }).click();
 
   await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
--- e2e/helpers/global-setup.ts
+++ e2e/helpers/global-setup.ts
@@ -1,10 +1,12 @@
 import { clearDatabase } from './db';
+import { setSettings } from './settings';
 
 /**
  *
  */
 async function globalSetup() {
   await clearDatabase();
+  await setSettings({});
 }
 
 export default globalSetup;
