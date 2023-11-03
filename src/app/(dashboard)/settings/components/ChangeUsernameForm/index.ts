49c6a8f9b7a5ce6bd83d91ce2783b82993f1073e
Nicolas Meienberger <github@thisprops.com>
2023-11-03 21:00:11 +0100

    feat(client): add form for change email in settings page

---
+++ src/app/(dashboard)/settings/components/ChangeUsernameForm/index.ts
@@ -0,0 +1,1 @@
+export { ChangeUsernameForm } from './ChangeUsernameForm';
--- src/app/(dashboard)/settings/components/SecurityContainer/SecurityContainer.tsx
+++ src/app/(dashboard)/settings/components/SecurityContainer/SecurityContainer.tsx
@@ -1,17 +1,24 @@
 'use client';
 
 import React from 'react';
-import { IconLock, IconKey } from '@tabler/icons-react';
+import { IconLock, IconKey, IconUser } from '@tabler/icons-react';
 import { useTranslations } from 'next-intl';
 import { OtpForm } from '../OtpForm';
 import { ChangePasswordForm } from '../ChangePasswordForm';
+import { ChangeUsernameForm } from '../ChangeUsernameForm';
 
-export const SecurityContainer = (props: { totpEnabled: boolean }) => {
-  const { totpEnabled } = props;
+export const SecurityContainer = (props: { totpEnabled: boolean; username?: string }) => {
+  const { totpEnabled, username } = props;
   const t = useTranslations('settings.security');
 
   return (
     <div className="card-body">
+      <div className="d-flex">
+        <IconUser className="me-2" />
+        <h2>{t('change-username.title')}</h2>
+      </div>
+      <p className="text-muted">{t('change-username.subtitle')}</p>
+      <ChangeUsernameForm username={username} />
       <div className="d-flex">
         <IconKey className="me-2" />
         <h2>{t('change-password-title')}</h2>
