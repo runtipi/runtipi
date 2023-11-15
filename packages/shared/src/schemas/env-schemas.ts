09461527ee26f1f8324cdbad13fc159cdca34e4e
Stavros <steveiliop56@gmail.com>
2023-11-15 22:16:10 +0200

    feat(cli): set custom postgres port in config.json (#918)
    * feat(cli): set custom postgres port in config.json
    
    * fix(cli): small misspelling error.
    
    * fix(cli): misspelling error again
    
    Co-authored-by: Nicolas Meienberger <47644445+meienberger@users.noreply.github.com>
    
    * fix(cli): add required env vard to schemas.
    
    * fix(cli): add default port to schema
    
    * fix(cli): add requested changes
    
    * fix(cli): move posgresPort to pick
    
    ---------
    
    Co-authored-by: Nicolas Meienberger <47644445+meienberger@users.noreply.github.com>

--- packages/shared/src/schemas/env-schemas.ts
+++ packages/shared/src/schemas/env-schemas.ts
@@ -63,5 +63,5 @@ export const envSchema = z.object({
 
 export const settingsSchema = envSchema
   .partial()
-  .pick({ dnsIp: true, internalIp: true, appsRepoUrl: true, domain: true, storagePath: true, localDomain: true, demoMode: true, guestDashboard: true })
+  .pick({ dnsIp: true, internalIp: true, postgresPort: true, appsRepoUrl: true, domain: true, storagePath: true, localDomain: true, demoMode: true, guestDashboard: true })
   .and(z.object({ port: z.number(), sslPort: z.number(), listenIp: z.string().ip().trim() }).partial());
