7218f7f80fd748dd3878f2e48c5b5d764f4a95f0
Nicolas Meienberger <github@thisprops.com>
2023-12-19 21:31:50 +0100

    ci(e2e): create state folder before trying to set file

--- e2e/helpers/settings.ts
+++ e2e/helpers/settings.ts
@@ -4,5 +4,8 @@ import { z } from 'zod';
 import { settingsSchema } from '@runtipi/shared';
 
 export const setSettings = async (settings: z.infer<typeof settingsSchema>) => {
+  // Create state folder if it doesn't exist
+  await promises.mkdir(path.join(__dirname, '../../state'), { recursive: true });
+
   await promises.writeFile(path.join(__dirname, '../../state/settings.json'), JSON.stringify(settings));
 };
