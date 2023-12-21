b95a2b1d3e70ec2be323a0d244bab903d3ab3622
Nicolas Meienberger <github@thisprops.com>
2023-12-21 14:38:07 +0100

    test(e2e): increase timeout of app test

--- e2e/0003-apps.spec.ts
+++ e2e/0003-apps.spec.ts
@@ -19,6 +19,7 @@ test.beforeEach(async ({ page, isMobile }) => {
 });
 
 test('user can install and uninstall app', async ({ page, context }) => {
+  test.slow();
   // Install app
   await page.getByRole('button', { name: 'Install' }).click();
 
@@ -31,7 +32,10 @@ test('user can install and uninstall app', async ({ page, context }) => {
   await expect(page.getByText('App installed successfully')).toBeVisible();
 
   await page.getByTestId('app-details').getByRole('button', { name: 'Open' }).press('ArrowDown');
-  const [newPage] = await Promise.all([context.waitForEvent('page'), await page.getByRole('menuitem', { name: `${process.env.SERVER_IP}:8000` }).click()]);
+  const [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    await page.getByRole('menuitem', { name: `${process.env.SERVER_IP}:8000` }).click(),
+  ]);
 
   await newPage.waitForLoadState();
   await expect(newPage.getByText('Hello World')).toBeVisible();
