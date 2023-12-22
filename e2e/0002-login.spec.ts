c6eb14407404b8b0f822ac75a7b62855785fd0fc
Nicolas Meienberger <github@thisprops.com>
2023-12-22 16:58:06 +0100

    test(e2e): add reset password test

--- e2e/0002-login.spec.ts
+++ e2e/0002-login.spec.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { test, expect } from '@playwright/test';
 import { loginUser, createTestUser } from './fixtures/fixtures';
 import { testUser } from './helpers/constants';
@@ -23,4 +24,34 @@ test('user can logout', async ({ page }) => {
   await page.getByTestId('logout-button').click();
 
   await expect(page.getByText('Login to your account')).toBeVisible();
+});
+
+test('user can reset their password', async ({ page }) => {
+  // Create password change request
+  await fs.promises.unlink('./state/password-change-request');
+
+  await createTestUser();
+  await page.goto('/login');
+
+  await page.getByRole('link', { name: 'Forgot password?' }).click();
+
+  await expect(page.getByText('./runtipi-cli reset-password')).toBeVisible();
+  await fs.promises.writeFile('./state/password-change-request', '');
+
+  await page.reload();
+
+  await expect(page.getByRole('heading', { name: 'Reset your password' })).toBeVisible();
+  await page.getByPlaceholder('Your new password', { exact: true }).fill('new-password');
+  await page.getByPlaceholder('Confirm your new password').fill('new-password');
+
+  await page.getByRole('button', { name: 'Reset password' }).click();
+
+  await page.getByRole('button', { name: 'Back to login' }).click();
+
+  await page.getByPlaceholder('you@example.com').fill(testUser.email);
+  await page.getByPlaceholder('Your password').fill('new-password');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
 });
