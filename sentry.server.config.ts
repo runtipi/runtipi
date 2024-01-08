2342f83ecff46faa48105026fd03fa17ae743fbc
Nicolas Meienberger <github@thisprops.com>
2024-01-08 21:06:49 +0100

    refactor(error-monitoring): make sentry opt-in

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,16 +2,15 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { cleanseErrorData } from '@runtipi/shared/src/helpers/error-helpers';
 import * as Sentry from '@sentry/nextjs';
 import { getConfig } from '@/server/core/TipiConfig';
+import { cleanseErrorData } from '@runtipi/shared/src/helpers/error-helpers';
 
 if (getConfig().allowErrorMonitoring) {
   Sentry.init({
     environment: getConfig().NODE_ENV,
     dsn: 'https://7a73d72f886948478b55621e7b92c3c7@o4504242900238336.ingest.sentry.io/4504826587971584',
     debug: getConfig().NODE_ENV === 'development',
-    enableTracing: false,
     beforeSend: cleanseErrorData,
   });
 }
