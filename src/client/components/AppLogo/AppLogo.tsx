89541396596fc6e785e5ed353d89510187b5e0e7
Nicolas Meienberger <github@thisprops.com>
2024-01-13 17:31:01 +0100

    chore: move slightly app logo svg path to cover more space

--- src/client/components/AppLogo/AppLogo.tsx
+++ src/client/components/AppLogo/AppLogo.tsx
@@ -9,7 +9,7 @@ export const AppLogo: React.FC<{ id?: string; size?: number; className?: string;
     <div aria-label={alt} className={clsx(styles.dropShadow, className)} style={{ width: size, height: size }}>
       <svg width={size} height={size} viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
         <mask id="mask0" maskUnits="userSpaceOnUse" x="0" y="0" width="200" height="200">
-          <path fillRule="evenodd" clipRule="evenodd" d="M0 100C0 0 0 0 100 0S200 0 200 100 200 200 100 200 0 200 0 100" fill="white" />
+          <path fillRule="evenodd" clipRule="evenodd" d="M-1 100C0 0 0 0 100 0S200 0 200 100 200 200 100 200 0 200 0 100" fill="white" />
         </mask>
         <image href={logoUrl} mask="url(#mask0)" width="200" height="200" />
       </svg>
