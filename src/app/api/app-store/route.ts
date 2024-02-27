7bb07e91e2c0a9fdd9fd630696d3e061dc4e3b45
Nicolas Meienberger <github@thisprops.com>
2024-02-27 21:05:19 +0100

    feat(apps): create search api and service

---
+++ src/app/api/app-store/route.ts
@@ -0,0 +1,22 @@
+import { AppServiceClass } from '@/server/services/apps/apps.service';
+
+const getApps = async (searchParams: URLSearchParams) => {
+  const search = searchParams.get('search');
+  const pageSize = searchParams.get('pageSize') || 18;
+  const category = searchParams.get('category');
+  const cursor = searchParams.get('cursor');
+
+  const appsService = new AppServiceClass();
+
+  return appsService.searchApps({ search, category, pageSize: Number(pageSize), cursor });
+};
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const apps = await getApps(searchParams);
+
+  return new Response(JSON.stringify(apps), { headers: { 'content-type': 'application/json' } });
+}
+
+export type AppStoreApiResponse = Awaited<ReturnType<typeof getApps>>;
