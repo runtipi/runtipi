5ee5f19d2a06a94a5fd92a53d09d0ded82f780e7
Stavros <steveiliop56@gmail.com>
2024-03-06 22:09:05 +0200

    feat: add production dockerfile

---
+++ start.prod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Start worker
+cd worker/
+pm2 start index.js --ignore-watch="/worker" --name worker -- start
+
+# Wait for http://localhost:5000/healthcheck to return OK with a maximum of 5 retries
+while true; do
+    if [[ "$(curl -s http://localhost:5000/worker-api/healthcheck)" == "OK" ]]; then
+        break
+    fi
+    sleep 1
+done
+
+# Start apps
+cd /dash
+pm2 start npm --ignore-watch="/dash" --name dashboard -- run start
+
+# Log apps realtime
+pm2 logs
