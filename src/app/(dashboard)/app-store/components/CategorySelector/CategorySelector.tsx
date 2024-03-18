2c003f64dd5986271e7bb9b27f61c62deb4b242e
Nicolas Meienberger <github@thisprops.com>
2024-03-18 18:45:41 +0100

    refactor(category-select): change from react-select to @radix for dark mode support

--- src/app/(dashboard)/app-store/components/CategorySelector/CategorySelector.tsx
+++ src/app/(dashboard)/app-store/components/CategorySelector/CategorySelector.tsx
@@ -1,114 +1,45 @@
-import { Icon } from '@tabler/icons-react';
-import React from 'react';
-import Select, { SingleValue, OptionProps, ControlProps, components } from 'react-select';
+import React, { useState } from 'react';
+import { Select, SelectItem, SelectValue, SelectContent, SelectTrigger } from '@/components/ui/Select';
 import { useTranslations } from 'next-intl';
 import { AppCategory } from '@runtipi/shared';
-import { useUIStore } from '@/client/state/uiStore';
 import { iconForCategory } from '../../helpers/table.helpers';
 
-const { Option, Control } = components;
-
-interface IProps {
+interface Props {
   onSelect: (value?: AppCategory) => void;
   className?: string;
   initialValue?: AppCategory;
 }
 
-type OptionsType = { value: AppCategory; label: string; icon: Icon };
-
-const IconOption = (props: OptionProps<OptionsType>) => {
-  const { data } = props;
-  const { icon: CategoryIcon, label } = data;
-  return (
-    <Option {...props}>
-      <>
-        <CategoryIcon size={20} />
-        <span style={{ marginLeft: 10 }}>{label}</span>
-      </>
-    </Option>
-  );
-};
-
-const ControlComponent = (props: ControlProps<OptionsType>) => {
-  const { children, ...rest } = props;
-  const { getValue } = props;
-
-  const value = getValue()[0];
-
-  if (value?.icon) {
-    return (
-      <Control {...rest}>
-        <value.icon className="ms-2" size={20} />
-        {children}
-      </Control>
-    );
-  }
-
-  return <Control {...rest}> {children}</Control>;
-};
-
-export const CategorySelector: React.FC<IProps> = ({ onSelect, className, initialValue }) => {
+export const CategorySelector = ({ onSelect, className, initialValue }: Props) => {
   const t = useTranslations();
-  const { darkMode } = useUIStore();
-  const options: OptionsType[] = iconForCategory.map((category) => ({
+  const options = iconForCategory.map((category) => ({
     value: category.id,
     label: t(`APP_CATEGORY_${category.id.toUpperCase() as Uppercase<typeof category.id>}`),
     icon: category.icon,
   }));
 
-  const [value, setValue] = React.useState<OptionsType | null>(options.find((o) => o.value === initialValue) || null);
+  const [value, setValue] = useState(initialValue);
 
-  const handleChange = (option: SingleValue<OptionsType>) => {
-    setValue(option as OptionsType);
-    onSelect(option?.value);
+  const handleChange = (option: AppCategory) => {
+    setValue(option);
+    onSelect(option);
   };
 
-  const color = darkMode ? '#fff' : '#1a2234';
-  const bgColor = darkMode ? '#1a2234' : '#fff';
-  const borderColor = darkMode ? '#243049' : '#e5e5e5';
-
   return (
-    <Select<OptionsType>
-      isClearable
-      className={className}
-      value={value}
-      styles={{
-        menu: (provided: object) => ({
-          ...provided,
-          backgroundColor: bgColor,
-          color,
-        }),
-        control: (provided: object) => ({
-          ...provided,
-          backgroundColor: bgColor,
-          color,
-          borderColor,
-        }),
-        option: (provided: object, state: { isFocused: boolean }) => ({
-          ...provided,
-          backgroundColor: state.isFocused ? '#243049' : bgColor,
-          color: state.isFocused ? '#fff' : color,
-        }),
-        singleValue: (provided: object) => ({
-          ...provided,
-          color,
-          fontSize: '0.8rem',
-        }),
-        placeholder: (provided: object) => ({
-          ...provided,
-          color: '#a5a9b1',
-          fontSize: '0.8rem',
-        }),
-      }}
-      components={{
-        Option: IconOption,
-        Control: ControlComponent,
-      }}
-      onChange={handleChange}
-      defaultValue={[]}
-      name="categories"
-      options={options}
-      placeholder={t('APP_STORE_CATEGORY_PLACEHOLDER')}
-    />
+    <Select value={value} onValueChange={handleChange}>
+      <SelectTrigger value={value} className={className}>
+        <SelectValue placeholder={t('APP_INSTALL_FORM_CHOOSE_OPTION')} />
+      </SelectTrigger>
+      <SelectContent>
+        {options?.map(({ value: category, icon: CategoryIcon, label }) => (
+          <SelectItem key={category} value={category}>
+            <span className="d-flex gap-2">
+              <CategoryIcon size={20} />
+              {label}
+            </span>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
