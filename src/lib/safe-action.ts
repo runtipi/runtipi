d45cd787f7621d175e9ae7b327728e07c3091ae1
Nicolas Meienberger <github@thisprops.com>
2024-03-24 15:48:01 +0100

    chore(server-error): print only message in console

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -3,7 +3,7 @@ import { createSafeActionClient } from 'next-safe-action';
 export const action = createSafeActionClient({
   handleReturnedServerError: (e) => {
     // eslint-disable-next-line no-console
-    console.error('Error from server', e);
+    console.error('Error from server', e.message);
 
     return e.message || 'An unexpected error occurred';
   },
