6d3eaf542ceb3ac36e0792649d14ea68f91d46b9
Jorge Montejo <jorgemon.lopez@gmail.com>
2024-04-11 13:22:01 +0000

    change tab when not running & logs options

--- src/app/(dashboard)/app-store/[id]/components/AppDetailsTabs/LogsTerminal.tsx
+++ src/app/(dashboard)/app-store/[id]/components/AppDetailsTabs/LogsTerminal.tsx
@@ -10,6 +10,8 @@ export const LogsTerminal = ({ appId }: { appId: string }) => {
   let nextId = 0;
   const [logs, setLogs] = useState<{ id: number, text: string }[]>([]);
   const [follow, setFollow] = useState<boolean>(true);
+  const [maxLines, setMaxLines] = useState<number>(300);
+  const [wrapLines, setWrapLines] = useState<boolean>(false);
   const ref = useRef<HTMLPreElement>(null);
 
   useSocketEmit({ type: 'viewLogs', data: { appId }, emitOnDisconnect: 'stopLogs' });
@@ -18,16 +20,11 @@ export const LogsTerminal = ({ appId }: { appId: string }) => {
     if (ref.current && follow) {
       ref.current.scrollTop = ref.current.scrollHeight;
     }
-  }, [logs, follow]);
-
-  const handleChangeFollow = () => {
-    setFollow(!follow);
-  }
+  }, [logs, follow, wrapLines]);
 
   useSocket({
     selector: { type: 'logs' },
     onEvent: (event, data) => {
-      console.log(data, event);
       data.lines.forEach((line) => {
         setLogs(prevLogs => [
           ...prevLogs,
@@ -40,20 +37,51 @@ export const LogsTerminal = ({ appId }: { appId: string }) => {
 
   return (
     <div>
-        <label className="form-check form-switch mt-1" htmlFor="follow-logs">
-          <input
-            id="follow-logs"
-            className="form-check-input"
-            type="checkbox"
-            checked={follow}
-            onChange={handleChangeFollow} />
-          <span className="form-check-label">Follow logs</span>
-        </label>
-        <pre id='log-terminal' className={clsx('mt-2', styles.logTerminal)} ref={ref} >
-          {logs.map((log) => (
-            <React.Fragment key={log.id}>{log.text}<br /></React.Fragment>
-          ))}
-        </pre>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <label className="form-check form-switch mt-1" htmlFor="follow-logs">
+              <input
+                id="follow-logs"
+                className="form-check-input"
+                type="checkbox"
+                checked={follow}
+                onChange={() => setFollow(!follow)} />
+              <span className="form-check-label">Follow logs</span>
+            </label>
+          </div>
+          <div className="col">
+            <label className="form-check form-switch mt-1" htmlFor="follow-logs">
+              <input
+                id="follow-logs"
+                className="form-check-input"
+                type="checkbox"
+                checked={wrapLines}
+                onChange={() => setWrapLines(!wrapLines)} />
+              <span className="form-check-label">Wrap lines</span>
+            </label>
+          </div>
+          <div className="col">
+            <div className="input-group mb-2">
+              <span className="input-group-text">Max lines:</span>
+              <input
+                id="max-lines"
+                type="number"
+                className="form-control"
+                value="300"
+                onChange={(e) => setMaxLines(parseInt(e.target.value, 10))} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <pre 
+        id='log-terminal' 
+        className={clsx('mt-2', styles.logTerminal, wrapLines ? styles.wrapLines : '')} 
+        ref={ref} >
+        {logs.map((log) => (
+          <React.Fragment key={log.id}>{log.text}<br /></React.Fragment>
+        ))}
+      </pre>
     </div>
   );
 }
