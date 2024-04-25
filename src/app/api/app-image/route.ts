20fafcd3487e2365a17e18f221df15e84a429c62
Nicolas Meienberger <github@thisprops.com>
2024-04-25 08:36:43 +0200

    refactor(app-image): change cache from 1 year to 1 day

--- src/app/api/app-image/route.ts
+++ src/app/api/app-image/route.ts
@@ -28,7 +28,7 @@ export async function GET(request: Request) {
 
     const file = fs.readFileSync(filePath);
 
-    return new Response(file, { headers: { 'content-type': 'image/jpeg', 'cache-control': 'public, max-age=31536000' } });
+    return new Response(file, { headers: { 'content-type': 'image/jpeg', 'cache-control': 'public, max-age=86400' } });
   } catch (error) {
     return handleApiError(error);
   }
