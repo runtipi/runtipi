a9ec7bdce52ddbf3f2ddcc75bdb17dfb31f28882
Nicolas Meienberger <github@thisprops.com>
2024-05-16 08:33:46 +0200

    fix(app-actions): missing open buttons with legacy app setup

--- src/app/(dashboard)/app-store/[id]/components/AppActions/AppActions.tsx
+++ src/app/(dashboard)/app-store/[id]/components/AppActions/AppActions.tsx
@@ -118,16 +118,20 @@ export const AppActions: React.FC<IProps> = ({
               {app.domain}
             </DropdownMenuItem>
           )}
-          <DropdownMenuItem onClick={() => onOpen('local_domain')}>
-            <IconLock className="text-muted me-2" size={16} />
-            {app.id}.{localDomain}
-          </DropdownMenuItem>
-          {!app.info.force_expose && (
-            <DropdownMenuItem onClick={() => onOpen('local')}>
-              <IconLockOff className="text-muted me-2" size={16} />
-              {hostname}:{app.info.port}
-            </DropdownMenuItem>
-          )}
+          {app.exposedLocal ||
+            (!info.dynamic_config && (
+              <DropdownMenuItem onClick={() => onOpen('local_domain')}>
+                <IconLock className="text-muted me-2" size={16} />
+                {app.id}.{localDomain}
+              </DropdownMenuItem>
+            ))}
+          {app.openPort ||
+            (!info.dynamic_config && (
+              <DropdownMenuItem onClick={() => onOpen('local')}>
+                <IconLockOff className="text-muted me-2" size={16} />
+                {hostname}:{app.info.port}
+              </DropdownMenuItem>
+            ))}
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
