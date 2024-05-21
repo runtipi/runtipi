31389caf20e332ce65163768fd354cafe8044bfc
Nicolas Meienberger <github@thisprops.com>
2024-05-21 20:04:53 +0200

    chore(watcher): pull repo only in non-dev mode

--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -75,9 +75,12 @@ const main = async () => {
     if (!clone.success) {
       logger.error(`Failed to clone repo ${envMap.get('APPS_REPO_URL') as string}`);
     }
-    const pull = await repoExecutors.pullRepo(envMap.get('APPS_REPO_URL') as string);
-    if (!pull.success) {
-      logger.error(`Failed to pull repo ${envMap.get('APPS_REPO_URL') as string}`);
+
+    if (process.env.NODE_ENV !== 'development') {
+      const pull = await repoExecutors.pullRepo(envMap.get('APPS_REPO_URL') as string);
+      if (!pull.success) {
+        logger.error(`Failed to pull repo ${envMap.get('APPS_REPO_URL') as string}`);
+      }
     }
 
     logger.info('Starting queue...');
@@ -97,6 +100,7 @@ const main = async () => {
     });
     logger.info('Obliterating queue...');
     await queue.drain(true);
+    await repeatQueue.drain(true);
 
     // Scheduled jobs
     if (process.env.NODE_ENV === 'production') {
