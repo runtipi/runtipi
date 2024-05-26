65546aef951f2a5408c1a2c25a3485e1038c8eea
Nicolas Meienberger <github@thisprops.com>
2024-05-26 16:10:49 +0200

    chore: various cleanups and deprecations fixes

--- src/app/components/ClientProviders/ClientProviders.tsx
+++ src/app/components/ClientProviders/ClientProviders.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React, { ComponentProps } from 'react';
-import { CookiesProvider } from 'next-client-cookies';
+import React from 'react';
 import { AbstractIntlMessages, NextIntlClientProvider } from 'next-intl';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from './ThemeProvider';
@@ -9,7 +8,6 @@ import { SocketProvider } from './SocketProvider/SocketProvider';
 
 type Props = {
   children: React.ReactNode;
-  cookies: ComponentProps<typeof CookiesProvider>['value'];
   initialTheme?: string;
   locale?: string;
   messages: AbstractIntlMessages;
@@ -17,18 +15,14 @@ type Props = {
 
 const queryClient = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } });
 
-export const ClientProviders = ({ children, initialTheme, cookies, locale, messages }: Props) => {
+export const ClientProviders = ({ children, initialTheme, locale, messages }: Props) => {
   return (
     <QueryClientProvider client={queryClient}>
       <NextIntlClientProvider locale={locale} messages={messages} timeZone={Intl.DateTimeFormat().resolvedOptions().timeZone}>
         <SocketProvider>
-          <CookiesProvider value={cookies}>
-            <ThemeProvider initialTheme={initialTheme}>{children}</ThemeProvider>
-          </CookiesProvider>
+          <ThemeProvider initialTheme={initialTheme}>{children}</ThemeProvider>
         </SocketProvider>
       </NextIntlClientProvider>
     </QueryClientProvider>
   );
 };
-
-export const ClientCookiesProvider: typeof CookiesProvider = (props) => <CookiesProvider {...props} />;
