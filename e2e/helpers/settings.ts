32992597658c9daf1325ca3cd6c93b676bce5e36
Nicolas Meienberger <47644445+meienberger@users.noreply.github.com>
2024-05-26 14:21:34 +0200

    ci(e2e): replace usages of old e2e workflow (#1436)

--- e2e/helpers/settings.ts
+++ e2e/helpers/settings.ts
@@ -2,52 +2,34 @@ import { promises } from 'fs';
 import { z } from 'zod';
 import { settingsSchema } from '@runtipi/shared';
 import { pathExists } from '@runtipi/shared/node';
-import { execRemoteCommand } from './write-remote-file';
+import path from 'path';
+import { BASE_PATH } from './constants';
 
 export const setSettings = async (settings: z.infer<typeof settingsSchema>) => {
-  if (process.env.REMOTE === 'true') {
-    await execRemoteCommand(`mkdir -p ./runtipi/state`);
-    await execRemoteCommand(`echo '${JSON.stringify(settings)}' > ./runtipi/state/settings.json`);
-  } else {
-    // Create state folder if it doesn't exist
-    await promises.mkdir('./state', { recursive: true });
-
-    await promises.writeFile('./state/settings.json', JSON.stringify(settings));
-  }
+  await promises.mkdir(path.join(BASE_PATH, 'state'), { recursive: true });
+  await promises.writeFile(path.join(BASE_PATH, 'state', 'settings.json'), JSON.stringify(settings));
 };
 
 export const setPassowrdChangeRequest = async () => {
-  if (process.env.REMOTE === 'true') {
-    // Write date in ms to file
-    await execRemoteCommand('touch ./runtipi/state/password-change-request && echo $(date +%s) >> ./runtipi/state/password-change-request');
-  } else {
-    await promises.writeFile('./state/password-change-request', `${new Date().getTime() / 1000}`);
-  }
+  await promises.writeFile(path.join(BASE_PATH, 'state', 'password-change-request'), `${new Date().getTime() / 1000}`);
 };
 
 export const unsetPasswordChangeRequest = async () => {
-  if (process.env.REMOTE === 'true') {
-    await execRemoteCommand('rm ./runtipi/state/password-change-request');
-  } else if (await pathExists('./state/password-change-request')) {
-    await promises.unlink('./state/password-change-request');
+  const requestPath = path.join(BASE_PATH, 'state', 'password-change-request');
+  if (await pathExists(requestPath)) {
+    await promises.unlink(requestPath);
   }
 };
 
 export const setWelcomeSeen = async (seen: boolean) => {
-  if (seen && process.env.REMOTE === 'true') {
-    return execRemoteCommand('touch ./runtipi/state/seen-welcome');
+  const seenPath = path.join(BASE_PATH, 'state', 'seen-welcome');
+
+  if (seen && !(await pathExists(seenPath))) {
+    return promises.writeFile(seenPath, '');
   }
 
-  if (!seen && process.env.REMOTE === 'true') {
-    return execRemoteCommand('rm ./runtipi/state/seen-welcome');
-  }
-
-  if (seen && !(await pathExists('./state/seen-welcome'))) {
-    return promises.writeFile('./state/seen-welcome', '');
-  }
-
-  if (!seen && (await pathExists('./state/seen-welcome'))) {
-    return promises.unlink('./state/seen-welcome');
+  if (!seen && (await pathExists(seenPath))) {
+    return promises.unlink(seenPath);
   }
 
   return Promise.resolve();
