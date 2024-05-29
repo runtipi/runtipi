88597360e301bb528befabf49afb99269d5fb086
Nicolas Meienberger <47644445+meienberger@users.noreply.github.com>
2024-05-29 20:46:54 +0200

    feat: view app logs in dashboard (#1445)
    * socket events for docker logs
    
    * auto-scroll with ref
    
    * add log terminal with auto scroll
    
    * remove console.logs
    
    * increase initial lines to 25
    
    * remove more console logs
    
    * useSocketEmit
    
    * emit on disconect & hide tab if not running
    
    * change tab when not running & logs options
    
    * logs max lines
    
    * logs emit
    
    * remove console logs
    
    * refactor(logs-socket): consolidate & reduce state usage
    
    * useTranslations in logs tab
    
    * remove wrapLines from useEffect
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * order file & add spanish translations
    
    * chore: fix tsc issues
    
    ---------
    
    Co-authored-by: Jorge Montejo <jorgemon.lopez@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/app/(dashboard)/app-store/[id]/components/AppDetailsContainer/AppDetailsContainer.tsx
+++ src/app/(dashboard)/app-store/[id]/components/AppDetailsContainer/AppDetailsContainer.tsx
@@ -236,7 +236,7 @@ export const AppDetailsContainer: React.FC<AppDetailsContainerProps> = ({ app, l
           />
         </div>
       </div>
-      <AppDetailsTabs info={app.info} />
+      <AppDetailsTabs info={app.info} status={optimisticStatus}/>
     </div>
   );
 };
---
+++ src/app/(dashboard)/app-store/[id]/components/AppDetailsTabs/LogsTerminal.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import React, { useEffect, useRef, useState } from 'react';
+import { useTranslations } from 'next-intl';
+import { useSocket } from '@/lib/socket/useSocket';
+import clsx from 'clsx';
+import styles from './LogsTerminal.module.scss';
+
+export const LogsTerminal = ({ appId }: { appId: string }) => {
+  const t = useTranslations();
+
+  let nextId = 0;
+  const [logs, setLogs] = useState<{ id: number; text: string }[]>([]);
+  const [follow, setFollow] = useState<boolean>(true);
+  const [maxLines, setMaxLines] = useState<number>(300);
+  const [wrapLines, setWrapLines] = useState<boolean>(false);
+  const ref = useRef<HTMLPreElement>(null);
+
+  useEffect(() => {
+    if (ref.current && follow) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  }, [logs, follow]);
+
+  useSocket({
+    selector: { type: 'logs', event: 'newLogs', data: { property: 'appId', value: appId } },
+    onCleanup: () => setLogs([]),
+    emitOnConnect: { event: 'viewLogs', data: { appId } },
+    emitOnDisconnect: { event: 'stopLogs', data: { appId } },
+    onEvent: (_, data) => {
+      setLogs((prevLogs) => {
+        if (!data.lines) {
+          return prevLogs;
+        }
+        const newLogs = [...prevLogs, ...data.lines.map((line) => ({ id: nextId++, text: line.trim() }))];
+        if (newLogs.length > maxLines) {
+          return newLogs.slice(newLogs.length - maxLines);
+        }
+        return newLogs;
+      });
+    },
+  });
+
+  const updateMaxLines = (lines: number) => {
+    const linesToKeep = Math.max(1, lines);
+    setMaxLines(linesToKeep);
+    setLogs(logs.slice(logs.length - linesToKeep));
+  };
+
+  return (
+    <div>
+      <div className="row d-flex align-items-center ps-1">
+        <div className="col">
+          <label className="form-check form-switch mt-1" htmlFor="follow-logs">
+            <input id="follow-logs" className="form-check-input" type="checkbox" checked={follow} onChange={() => setFollow(!follow)} />
+            <span className="form-check-label">{t('APP_LOGS_TAB_FOLLOW')}</span>
+          </label>
+        </div>
+        <div className="col">
+          <label className="form-check form-switch mt-1" htmlFor="follow-logs">
+            <input id="follow-logs" className="form-check-input" type="checkbox" checked={wrapLines} onChange={() => setWrapLines(!wrapLines)} />
+            <span className="form-check-label">{t('APP_LOGS_TAB_WRAP_LINES')}</span>
+          </label>
+        </div>
+        <div className="col">
+          <div className="input-group mb-2">
+            <span className="input-group-text">{t('APP_LOGS_TAB_MAX_LINES')}</span>
+            <input
+              id="max-lines"
+              type="number"
+              className="form-control"
+              value={maxLines}
+              onChange={(e) => updateMaxLines(parseInt(e.target.value, 10))}
+            />
+          </div>
+        </div>
+      </div>
+      <pre id="log-terminal" className={clsx('mt-2', styles.logTerminal, { [styles.wrapLines || '']: wrapLines })} ref={ref}>
+        {logs.map((log) => (
+          <React.Fragment key={log.id}>
+            {log.text}
+            <br />
+          </React.Fragment>
+        ))}
+      </pre>
+    </div>
+  );
+};
