faf757de22b041161fcdcf57920b940a88155ea0
Nicolas Meienberger <github@thisprops.com>
2024-06-02 21:16:23 +0200

    fix(logs): maxlines not updated in closure

--- src/app/(dashboard)/settings/components/LogsContainer/LogsContainer.tsx
+++ src/app/(dashboard)/settings/components/LogsContainer/LogsContainer.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSocket } from '@/lib/socket/useSocket';
 import { LogsTerminal } from 'src/app/components/LogsTerminal/LogsTerminal';
 
 export const LogsContainer = () => {
   let nextId = 0;
   const [logs, setLogs] = useState<{ id: number; text: string }[]>([]);
-  const [maxLines, setMaxLines] = useState<number>(300);
+  const maxLines = useRef(300);
 
   useSocket({
     selector: { type: 'runtipi-logs', event: 'newLogs' },
     onCleanup: () => setLogs([]),
-    emitOnConnect: { type: 'runtipi-logs-init', event: 'initLogs', data: { maxLines } },
+    emitOnConnect: { type: 'runtipi-logs-init', event: 'initLogs', data: { maxLines: maxLines.current } },
     emitOnDisconnect: { type: 'runtipi-logs', event: 'stopLogs', data: {} },
     onEvent: (_, data) => {
       setLogs((prevLogs) => {
@@ -20,8 +20,8 @@ export const LogsContainer = () => {
           return prevLogs;
         }
         const newLogs = [...prevLogs, ...data.lines.map((line) => ({ id: nextId++, text: line.trim() }))];
-        if (newLogs.length > maxLines) {
-          return newLogs.slice(newLogs.length - maxLines);
+        if (newLogs.length > maxLines.current) {
+          return newLogs.slice(newLogs.length - maxLines.current);
         }
         return newLogs;
       });
@@ -30,9 +30,9 @@ export const LogsContainer = () => {
 
   const updateMaxLines = (lines: number) => {
     const linesToKeep = Math.max(1, lines);
-    setMaxLines(linesToKeep);
+    maxLines.current = linesToKeep;
     setLogs(logs.slice(logs.length - linesToKeep));
   };
 
-  return <LogsTerminal logs={logs} maxLines={maxLines} onMaxLinesChange={updateMaxLines} />;
+  return <LogsTerminal logs={logs} maxLines={maxLines.current} onMaxLinesChange={updateMaxLines} />;
 };
