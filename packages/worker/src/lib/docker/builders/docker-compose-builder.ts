ab46e7b7b2f333dc2c4f3f91a4fc4b6c03ef6139
Nicolas Meienberger <47644445+meienberger@users.noreply.github.com>
2024-06-09 15:02:24 +0200

    refactor(docker-templates): use a builder pattern to improve readability and maintainability (#1472)

---
+++ packages/worker/src/lib/docker/builders/docker-compose-builder.ts
@@ -0,0 +1,38 @@
+import * as yaml from 'yaml';
+import { BuiltService } from './service-builder';
+
+interface Network {
+  key?: string;
+  name: string;
+  external: boolean;
+}
+
+export class DockerComposeBuilder {
+  private services: Record<string, BuiltService> = {};
+  private networks: Record<string, Network> = {};
+
+  addService(service: BuiltService) {
+    this.services[service.container_name] = service;
+    return this;
+  }
+
+  addServices(services: BuiltService[]) {
+    services.forEach((service) => this.addService(service));
+    return this;
+  }
+
+  addNetwork(network: Network) {
+    this.networks[network.key || network.name] = {
+      name: network.name,
+      external: network.external,
+    };
+    return this;
+  }
+
+  build() {
+    return yaml.stringify({
+      services: this.services,
+      networks: this.networks,
+    });
+  }
+}
