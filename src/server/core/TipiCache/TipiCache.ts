fbc1a91191c46d6a8ae7e41e96e805b803ce49a0
Nicolas Meienberger <github@thisprops.com>
2024-07-29 22:41:12 +0200

    refactor: introduce DI with logger

--- src/server/core/TipiCache/TipiCache.ts
+++ src/server/core/TipiCache/TipiCache.ts
@@ -1,23 +1,40 @@
 import { createClient, RedisClientType } from 'redis';
 import { TipiConfig } from '../TipiConfig';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '@runtipi/shared/node';
 
 const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
 
-export class TipiCacheClass {
+export interface ITipiCache {
+  set(key: string, value: string, expiration?: number): Promise<string | null>;
+  get(key: string): Promise<string | null>;
+  del(key: string): Promise<number>;
+  getByPrefix(prefix: string): Promise<Array<{ key: string; val: string | null }>>;
+  close(): Promise<string>;
+  ttl(key: string): Promise<number>;
+}
+
+@injectable()
+export class TipiCache {
   private client: RedisClientType;
 
-  constructor() {
+  constructor(@inject('ILogger') private logger: ILogger) {
     const client = createClient({
       url: `redis://${TipiConfig.getConfig().REDIS_HOST}:6379`,
       password: TipiConfig.getConfig().redisPassword,
     });
 
     this.client = client as RedisClientType;
+
+    this.client.on('error', (error) => {
+      this.logger.error('cache error', error);
+    });
   }
 
   private async getClient(): Promise<RedisClientType> {
     if (!this.client.isOpen) {
       await this.client.connect();
+      this.logger.info('connected to cache');
     }
     return this.client;
   }
@@ -55,6 +72,7 @@ export class TipiCacheClass {
   }
 
   public async close() {
+    this.logger.info('Closing cache connection');
     return this.client.quit();
   }
 
@@ -63,13 +81,3 @@ export class TipiCacheClass {
     return client.ttl(key);
   }
 }
-
-declare global {
-  // eslint-disable-next-line vars-on-top, no-var -- globalThis is not a module
-  var TipiCache: TipiCacheClass;
-}
-
-const tipiCacheSingleton = () => {
-  return new TipiCacheClass();
-};
-export const tipiCache = globalThis.TipiCache ?? tipiCacheSingleton();
