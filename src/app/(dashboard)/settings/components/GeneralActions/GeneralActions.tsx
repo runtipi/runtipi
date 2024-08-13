520174c80f413da28ab1e6098787abf72f556d1e
Stavros <steveiliop56@gmail.com>
2024-08-13 22:24:38 +0300

    chore: add some formatting

--- src/app/(dashboard)/settings/components/GeneralActions/GeneralActions.tsx
+++ src/app/(dashboard)/settings/components/GeneralActions/GeneralActions.tsx
@@ -1,23 +1,31 @@
-'use client';
+"use client";
 
-import { Markdown } from '@/components/Markdown';
-import { Button } from '@/components/ui/Button';
-import { IconStar } from '@tabler/icons-react';
-import { useTranslations } from 'next-intl';
-import React from 'react';
-import semver from 'semver';
+import { Markdown } from "@/components/Markdown";
+import { Button } from "@/components/ui/Button";
+import { IconCopy, IconStar } from "@tabler/icons-react";
+import { useTranslations } from "next-intl";
+import React from "react";
+import semver from "semver";
+import toast from "react-hot-toast";
 
-type Props = { version: { current: string; latest: string; body?: string | null } };
+type Props = {
+  version: { current: string; latest: string; body?: string | null };
+  jwtToken: string;
+};
 
 export const GeneralActions = (props: Props) => {
   const t = useTranslations();
-  const { version } = props;
 
-  const isLatest = semver.valid(version.current) && semver.valid(version.latest) && semver.gte(version.current, version.latest);
+  const { version, jwtToken } = props;
+
+  const isLatest =
+    semver.valid(version.current) &&
+    semver.valid(version.latest) &&
+    semver.gte(version.current, version.latest);
 
   const renderUpdate = () => {
     if (isLatest) {
-      return <Button disabled>{t('SETTINGS_ACTIONS_ALREADY_LATEST')}</Button>;
+      return <Button disabled>{t("SETTINGS_ACTIONS_ALREADY_LATEST")}</Button>;
     }
 
     return (
@@ -40,12 +48,42 @@ export const GeneralActions = (props: Props) => {
 
   return (
     <div className="card-body">
-      <h2 className="mb-4">{t('SETTINGS_ACTIONS_TITLE')}</h2>
-      <h3 className="card-title mt-4">{t('SETTINGS_ACTIONS_CURRENT_VERSION', { version: version.current })}</h3>
+      <h2 className="mb-4">{t("SETTINGS_ACTIONS_TITLE")}</h2>
+      <h3 className="card-title mt-4">
+        {t("SETTINGS_ACTIONS_CURRENT_VERSION", { version: version.current })}
+      </h3>
       <p className="card-subtitle">
-        {isLatest ? t('SETTINGS_ACTIONS_STAY_UP_TO_DATE') : t('SETTINGS_ACTIONS_NEW_VERSION', { version: version.latest })}
+        {isLatest
+          ? t("SETTINGS_ACTIONS_STAY_UP_TO_DATE")
+          : t("SETTINGS_ACTIONS_NEW_VERSION", { version: version.latest })}
       </p>
       {renderUpdate()}
+      <h3 className="mt-4">{t("SETTINGS_ACTIONS_API_TOKEN")}</h3>
+      <p className="card-subtitle">
+        {t("SETTINGS_ACTIONS_API_TOKEN_DESCRIPTION")}
+      </p>
+      <div className="input-group">
+        <div className="input-group">
+          <input className="form-control" readOnly value={jwtToken} />
+          <button
+            className="btn btn-danger"
+            onClick={() => {
+              navigator.clipboard.writeText(jwtToken);
+              toast.success("API token copied to clipboard!");
+            }}
+          >
+            <IconCopy stroke={1.5} />
+          </button>
+        </div>
+      </div>
+      <div className="alert alert-warning mt-4" role="alert">
+        <h4 className="alert-title">
+          {t("SETTINGS_ACTIONS_API_TOKEN_WARNING_TITLE")}
+        </h4>
+        <div className="text-secondary">
+          {t("SETTINGS_ACTIONS_API_TOKEN_WARNING_SUBTITILE")}
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,45 +1,55 @@
-import { Tabs, TabsContent } from '@/components/ui/tabs';
-import { getTranslator } from '@/lib/get-translator';
-import { getUserFromCookie } from '@/server/common/session.helpers';
-import { TipiConfig } from '@/server/core/TipiConfig';
-import { SystemServiceClass } from '@/server/services/system';
-import type { Metadata } from 'next';
-import React from 'react';
-import { getCurrentLocale } from 'src/utils/getCurrentLocale';
-import { GeneralActions } from './components/GeneralActions';
-import { LogsContainer } from './components/LogsContainer';
-import { SecurityContainer } from './components/SecurityContainer';
-import { SettingsContainer } from './components/SettingsContainer';
-import { SettingsTabTriggers } from './components/SettingsTabTriggers';
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { getTranslator } from "@/lib/get-translator";
+import { getUserFromCookie } from "@/server/common/session.helpers";
+import { TipiConfig } from "@/server/core/TipiConfig";
+import { SystemServiceClass } from "@/server/services/system";
+import type { Metadata } from "next";
+import React from "react";
+import { getCurrentLocale } from "src/utils/getCurrentLocale";
+import { GeneralActions } from "./components/GeneralActions";
+import { LogsContainer } from "./components/LogsContainer";
+import { SecurityContainer } from "./components/SecurityContainer";
+import { SettingsContainer } from "./components/SettingsContainer";
+import { SettingsTabTriggers } from "./components/SettingsTabTriggers";
+import * as jwt from "jsonwebtoken";
 
 export async function generateMetadata(): Promise<Metadata> {
   const translator = await getTranslator();
 
   return {
-    title: `${translator('SETTINGS_TITLE')} - Tipi`,
+    title: `${translator("SETTINGS_TITLE")} - Tipi`,
   };
 }
 
-export default async function SettingsPage({ searchParams }: { searchParams: { tab: string } }) {
+export default async function SettingsPage({
+  searchParams,
+}: {
+  searchParams: { tab: string };
+}) {
   const { tab } = searchParams;
   const systemService = new SystemServiceClass();
   const version = await systemService.getVersion();
   const settings = TipiConfig.getSettings();
+  const config = TipiConfig.getConfig();
   const locale = getCurrentLocale();
   const user = await getUserFromCookie();
+  const jwtToken = jwt.sign({ skill: "issue" }, config.jwtSecret);
 
   return (
     <div className="card d-flex">
-      <Tabs defaultValue={tab || 'actions'}>
+      <Tabs defaultValue={tab || "actions"}>
         <SettingsTabTriggers />
         <TabsContent value="actions">
-          <GeneralActions version={version} />
+          <GeneralActions version={version} jwtToken={jwtToken} />
         </TabsContent>
         <TabsContent value="settings">
           <SettingsContainer initialValues={settings} currentLocale={locale} />
         </TabsContent>
         <TabsContent value="security">
-          <SecurityContainer totpEnabled={Boolean(user?.totpEnabled)} username={user?.username} />
+          <SecurityContainer
+            totpEnabled={Boolean(user?.totpEnabled)}
+            username={user?.username}
+          />
         </TabsContent>
         <TabsContent value="logs">
           <LogsContainer />
