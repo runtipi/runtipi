576c05f5699b028a7cae946e1093529f66613efb
Stavros <steveiliop56@gmail.com>
2024-08-14 21:56:57 +0300

    fix: remove timeout from backups

--- src/server/services/app-backup/commands/create-app-backup-command.ts
+++ src/server/services/app-backup/commands/create-app-backup-command.ts
@@ -1,11 +1,11 @@
-import type { EventDispatcher } from '@/server/core/EventDispatcher';
-import { Logger } from '@/server/core/Logger';
-import { TipiConfig } from '@/server/core/TipiConfig';
-import type { IAppQueries } from '@/server/queries/apps/apps.queries';
-import { TranslatedError } from '@/server/utils/errors';
-import type { AppStatus } from '@runtipi/db';
-import { appLifecycle } from '../../app-lifecycle/app-lifecycle.service';
-import type { AppBackupCommandParams, IAppBackupCommand } from './types';
+import type { EventDispatcher } from "@/server/core/EventDispatcher";
+import { Logger } from "@/server/core/Logger";
+import { TipiConfig } from "@/server/core/TipiConfig";
+import type { IAppQueries } from "@/server/queries/apps/apps.queries";
+import { TranslatedError } from "@/server/utils/errors";
+import type { AppStatus } from "@runtipi/db";
+import { appLifecycle } from "../../app-lifecycle/app-lifecycle.service";
+import type { AppBackupCommandParams, IAppBackupCommand } from "./types";
 
 export class CreateAppBackupCommand implements IAppBackupCommand {
   private queries: IAppQueries;
@@ -16,40 +16,45 @@ export class CreateAppBackupCommand implements IAppBackupCommand {
     this.eventDispatcher = params.eventDispatcher;
   }
 
-  private async sendEvent(appId: string, appStatusBeforeUpdate?: AppStatus): Promise<void> {
-    const { success, stdout } = await this.eventDispatcher.dispatchEventAsync(
-      { type: 'app', command: 'backup', appid: appId, form: {} },
-      1000 * 60 * 15, // 15 minutes
-    );
+  private async sendEvent(
+    appId: string,
+    appStatusBeforeUpdate?: AppStatus
+  ): Promise<void> {
+    const { success, stdout } = await this.eventDispatcher.dispatchEventAsync({
+      type: "app",
+      command: "backup",
+      appid: appId,
+      form: {},
+    });
 
     if (success) {
-      if (appStatusBeforeUpdate === 'running') {
-        await appLifecycle.executeCommand('startApp', { appId });
+      if (appStatusBeforeUpdate === "running") {
+        await appLifecycle.executeCommand("startApp", { appId });
       } else {
         await this.queries.updateApp(appId, { status: appStatusBeforeUpdate });
       }
 
-      await this.queries.updateApp(appId, { status: 'running' });
+      await this.queries.updateApp(appId, { status: "running" });
     } else {
       Logger.error(`Failed to backup app ${appId}: ${stdout}`);
-      await this.queries.updateApp(appId, { status: 'stopped' });
+      await this.queries.updateApp(appId, { status: "stopped" });
     }
   }
 
   async execute(params: { appId: string }): Promise<void> {
     if (TipiConfig.getConfig().demoMode) {
-      throw new TranslatedError('SERVER_ERROR_NOT_ALLOWED_IN_DEMO');
+      throw new TranslatedError("SERVER_ERROR_NOT_ALLOWED_IN_DEMO");
     }
 
     const { appId } = params;
     const app = await this.queries.getApp(appId);
 
     if (!app) {
-      throw new TranslatedError('APP_ERROR_APP_NOT_FOUND', { id: appId });
+      throw new TranslatedError("APP_ERROR_APP_NOT_FOUND", { id: appId });
     }
 
     // Run script
-    await this.queries.updateApp(appId, { status: 'backing_up' });
+    await this.queries.updateApp(appId, { status: "backing_up" });
 
     void this.sendEvent(appId, app.status);
   }
--- src/server/services/app-lifecycle/commands/update-app-command.ts
+++ src/server/services/app-lifecycle/commands/update-app-command.ts
@@ -1,14 +1,14 @@
-import { castAppConfig } from '@/lib/helpers/castAppConfig';
-import type { EventDispatcher } from '@/server/core/EventDispatcher';
-import { Logger } from '@/server/core/Logger';
-import { TipiConfig } from '@/server/core/TipiConfig';
-import type { IAppQueries } from '@/server/queries/apps/apps.queries';
-import { TranslatedError } from '@/server/utils/errors';
-import type { AppStatus } from '@runtipi/db';
-import type { AppEventFormInput } from '@runtipi/shared';
-import type { AppDataService } from '@runtipi/shared/node';
-import semver from 'semver';
-import type { AppLifecycleCommandParams, IAppLifecycleCommand } from './types';
+import { castAppConfig } from "@/lib/helpers/castAppConfig";
+import type { EventDispatcher } from "@/server/core/EventDispatcher";
+import { Logger } from "@/server/core/Logger";
+import { TipiConfig } from "@/server/core/TipiConfig";
+import type { IAppQueries } from "@/server/queries/apps/apps.queries";
+import { TranslatedError } from "@/server/utils/errors";
+import type { AppStatus } from "@runtipi/db";
+import type { AppEventFormInput } from "@runtipi/shared";
+import type { AppDataService } from "@runtipi/shared/node";
+import semver from "semver";
+import type { AppLifecycleCommandParams, IAppLifecycleCommand } from "./types";
 
 const FIFTEEN_MINUTES = 15 * 60 * 1000;
 
@@ -16,7 +16,7 @@ export class UpdateAppCommand implements IAppLifecycleCommand {
   private queries: IAppQueries;
   private eventDispatcher: EventDispatcher;
   private appDataService: AppDataService;
-  private executeOtherCommand: IAppLifecycleCommand['execute'];
+  private executeOtherCommand: IAppLifecycleCommand["execute"];
 
   constructor(params: AppLifecycleCommandParams) {
     this.queries = params.queries;
@@ -33,44 +33,62 @@ export class UpdateAppCommand implements IAppLifecycleCommand {
   }): Promise<void> {
     const { appId, form, appStatusBeforeUpdate, performBackup } = params;
 
-    const { success, stdout } = await this.eventDispatcher.dispatchEventAsync(
-      { type: 'app', command: 'update', appid: appId, form, performBackup },
-      performBackup ? FIFTEEN_MINUTES : undefined,
-    );
+    const { success, stdout } = await this.eventDispatcher.dispatchEventAsync({
+      type: "app",
+      command: "update",
+      appid: appId,
+      form,
+      performBackup,
+    });
 
     if (success) {
       const appInfo = await this.appDataService.getInstalledInfo(appId);
 
       await this.queries.updateApp(appId, { version: appInfo?.tipi_version });
 
-      if (appStatusBeforeUpdate === 'running') {
-        await this.executeOtherCommand('startApp', { appId });
+      if (appStatusBeforeUpdate === "running") {
+        await this.executeOtherCommand("startApp", { appId });
       } else {
         await this.queries.updateApp(appId, { status: appStatusBeforeUpdate });
       }
     } else {
-      await this.queries.updateApp(appId, { status: 'stopped' });
+      await this.queries.updateApp(appId, { status: "stopped" });
       Logger.error(`Failed to update app ${appId}: ${stdout}`);
     }
   }
 
-  async execute(params: { appId: string; performBackup: boolean }): Promise<void> {
+  async execute(params: {
+    appId: string;
+    performBackup: boolean;
+  }): Promise<void> {
     const { appId, performBackup } = params;
     const app = await this.queries.getApp(appId);
 
     if (!app) {
-      throw new TranslatedError('APP_ERROR_APP_NOT_FOUND', { id: appId });
+      throw new TranslatedError("APP_ERROR_APP_NOT_FOUND", { id: appId });
     }
 
     const { version } = TipiConfig.getConfig();
 
     const { minTipiVersion } = await this.appDataService.getUpdateInfo(appId);
-    if (minTipiVersion && semver.valid(version) && semver.lt(version, minTipiVersion)) {
-      throw new TranslatedError('APP_UPDATE_ERROR_MIN_TIPI_VERSION', { id: appId, minVersion: minTipiVersion });
+    if (
+      minTipiVersion &&
+      semver.valid(version) &&
+      semver.lt(version, minTipiVersion)
+    ) {
+      throw new TranslatedError("APP_UPDATE_ERROR_MIN_TIPI_VERSION", {
+        id: appId,
+        minVersion: minTipiVersion,
+      });
     }
 
-    await this.queries.updateApp(appId, { status: 'updating' });
+    await this.queries.updateApp(appId, { status: "updating" });
 
-    void this.sendEvent({ appId, form: castAppConfig(app.config), appStatusBeforeUpdate: app.status || 'missing', performBackup });
+    void this.sendEvent({
+      appId,
+      form: castAppConfig(app.config),
+      appStatusBeforeUpdate: app.status || "missing",
+      performBackup,
+    });
   }
 }
