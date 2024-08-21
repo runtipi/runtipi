9166a441554fdc97d7898fe746324da3ba4b6346
Sergio <77530549+sergi0g@users.noreply.github.com>
2024-08-21 19:13:03 +0300

    WIP

--- src/app/(dashboard)/app-store/components/CategorySelector/CategorySelector.tsx
+++ src/app/(dashboard)/app-store/components/CategorySelector/CategorySelector.tsx
@@ -3,6 +3,7 @@ import type { AppCategory } from '@runtipi/shared';
 import { useTranslations } from 'next-intl';
 import { useState } from 'react';
 import { iconForCategory } from '../../helpers/table.helpers';
+import { IconChevronDown, IconX } from '@tabler/icons-react';
 
 interface Props {
   onSelect: (value?: AppCategory) => void;
@@ -25,10 +26,18 @@ export const CategorySelector = ({ onSelect, className, initialValue }: Props) =
     onSelect(option);
   };
 
+  const handleClear = () => {
+    setValue(undefined);
+    onSelect(undefined);
+  }
+
   return (
     <Select value={value} onValueChange={handleChange}>
-      <SelectTrigger value={value} className={className}>
+      <SelectTrigger value={value} className={className} style={{paddingRight: '0.75rem', backgroundImage: 'none !important'}}>
         <SelectValue placeholder={t('APP_INSTALL_FORM_CHOOSE_OPTION')} />
+        <div style={{borderLeft: "1px solid #1f2e41", paddingLeft: "0.5rem"}} onClick={handleClear}>
+          {value ? <IconX size={18} color='white'/> : <IconChevronDown size={18} color='white'/>}
+        </div>
       </SelectTrigger>
       <SelectContent>
         {options?.map(({ value: category, icon: CategoryIcon, label }) => (
