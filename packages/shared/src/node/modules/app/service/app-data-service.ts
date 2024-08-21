41c0e333bce23e7d843b7ca8917a8503e2994b56
Sergio <77530549+sergi0g@users.noreply.github.com>
2024-08-21 12:34:31 +0300

    Sort backup list newest -> oldest

--- packages/shared/src/node/modules/app/service/app-data-service.ts
+++ packages/shared/src/node/modules/app/service/app-data-service.ts
@@ -71,7 +71,8 @@ export class AppDataService implements IAppDataService {
   public async getAppBackups(params: { appId: string; pageSize: number; page: number }) {
     const { appId, page, pageSize } = params;
     const backups = await this.dataAccessApp.listBackupsByAppId(appId);
-    backups.reverse();
+    backups.sort((a, b) => b.date.getTime() - a.date.getTime());
+    backups.map(({date}) => date.toISOString());
 
     const start = (page - 1) * pageSize;
     const end = start + pageSize;
