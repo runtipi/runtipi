bb938a178db366c78939449c43a9736ff49033ca
Stavros <steveiliop56@gmail.com>
2024-08-26 21:13:27 +0300

    chore: fix formatting

--- packages/worker/src/lib/system/system.helpers.test.ts
+++ packages/worker/src/lib/system/system.helpers.test.ts
@@ -1,67 +1,62 @@
-import fs from "node:fs";
-import path from "node:path";
-import { DATA_DIR } from "@/config/constants";
-import { faker } from "@faker-js/faker";
-import { envMapToString } from "@runtipi/shared";
-import { beforeEach, describe, expect, it } from "vitest";
-import { generateSystemEnvFile } from "./system.helpers";
+import fs from 'node:fs';
+import path from 'node:path';
+import { DATA_DIR } from '@/config/constants';
+import { faker } from '@faker-js/faker';
+import { envMapToString } from '@runtipi/shared';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { generateSystemEnvFile } from './system.helpers';
 
 const envMap = new Map();
 
 const prepareEnvFile = () => {
   const content = envMapToString(envMap);
-  fs.writeFileSync(path.join(DATA_DIR, ".env"), content);
+  fs.writeFileSync(path.join(DATA_DIR, '.env'), content);
 };
 
 beforeEach(() => {
   envMap.clear();
-  envMap.set("ROOT_FOLDER_HOST", "/path/to/root");
-  envMap.set("INTERNAL_IP", "10.0.0.1");
+  envMap.set('ROOT_FOLDER_HOST', '/path/to/root');
+  envMap.set('INTERNAL_IP', '10.0.0.1');
 });
 
-describe("generateSystemEnvFile()", async () => {
+describe('generateSystemEnvFile()', async () => {
   it("should create an empty env file and settigns file if they don't exist", async () => {
     // act
     await generateSystemEnvFile().catch(() => {});
 
     // assert
-    const envFileExists = fs.existsSync(path.join(DATA_DIR, ".env"));
+    const envFileExists = fs.existsSync(path.join(DATA_DIR, '.env'));
     expect(envFileExists).toBe(true);
 
-    const settingsFileExists = fs.existsSync(
-      path.join(DATA_DIR, "state", "settings.json")
-    );
-    const settingsFileContent = fs.readFileSync(
-      path.join(DATA_DIR, "state", "settings.json"),
-      "utf8"
-    );
+    const settingsFileExists = fs.existsSync(path.join(DATA_DIR, 'state', 'settings.json'));
+    const settingsFileContent = fs.readFileSync(path.join(DATA_DIR, 'state', 'settings.json'), 'utf8');
     expect(settingsFileExists).toBe(true);
-    expect(settingsFileContent).toBe("{}");
+    expect(settingsFileContent).toBe('{}');
   });
 
-  it("should throw an error if ROOT_FOLDER_HOST is not set in the env file", async () => {
+  it('should throw an error if ROOT_FOLDER_HOST is not set in the env file', async () => {
     // arrange
     envMap.clear();
 
     // act
     await generateSystemEnvFile().catch((err) => {
-      expect(err.message).toBe("ROOT_FOLDER_HOST not set in .env file");
+      expect(err.message).toBe('ROOT_FOLDER_HOST not set in .env file');
     });
   });
 
-  it("should throw an error if INTERNAL_IP is not set in the env file", async () => {
+  it('should throw an error if INTERNAL_IP is not set in the env file', async () => {
     // arrange
     envMap.clear();
-    envMap.set("ROOT_FOLDER_HOST", "/path/to/root");
+    envMap.set('ROOT_FOLDER_HOST', '/path/to/root');
     prepareEnvFile();
 
     // act
     await generateSystemEnvFile().catch((err) => {
-      expect(err.message).toBe("INTERNAL_IP not set in .env file");
+      expect(err.message).toBe('INTERNAL_IP not set in .env file');
     });
   });
 
-  it("should successfully generate the system env file with default values", async () => {
+  it('should successfully generate the system env file with default values', async () => {
     // arrange
     prepareEnvFile();
 
@@ -69,78 +64,70 @@ describe("generateSystemEnvFile()", async () => {
     const generated = await generateSystemEnvFile();
 
     // assert
-    expect(generated.get("APPS_REPO_ID")).toBe(
-      "29ca930bfdaffa1dfabf5726336380ede7066bc53297e3c0c868b27c97282903"
-    );
-    expect(generated.get("APPS_REPO_URL")).toBe(
-      "https://github.com/runtipi/runtipi-appstore"
-    );
-    expect(generated.get("JWT_SECRET")).toBeTruthy();
-    expect(generated.get("DOMAIN")).toBe("example.com");
-    expect(generated.get("RUNTIPI_APP_DATA_PATH")).toBe(
-      envMap.get("ROOT_FOLDER_HOST")
-    );
-    expect(generated.get("POSTGRES_HOST")).toBe("runtipi-db");
-    expect(generated.get("POSTGRES_DBNAME")).toBe("tipi");
-    expect(generated.get("POSTGRES_USERNAME")).toBe("tipi");
-    expect(generated.get("POSTGRES_PORT")).toBe("5432");
-    expect(generated.get("REDIS_HOST")).toBe("runtipi-redis");
-    expect(generated.get("DEMO_MODE")).toBe("false");
-    expect(generated.get("GUEST_DASHBOARD")).toBe("false");
-    expect(generated.get("LOCAL_DOMAIN")).toBe("tipi.lan");
-    expect(generated.get("ALLOW_AUTO_THEMES")).toBe("true");
-    expect(generated.get("ALLOW_ERROR_MONITORING")).toBe("false");
-    expect(generated.get("PERSIST_TRAEFIK_CONFIG")).toBe("false");
-    expect(generated.get("GIT_VERIFY_SSL")).toBe("true");
+    expect(generated.get('APPS_REPO_ID')).toBe('29ca930bfdaffa1dfabf5726336380ede7066bc53297e3c0c868b27c97282903');
+    expect(generated.get('APPS_REPO_URL')).toBe('https://github.com/runtipi/runtipi-appstore');
+    expect(generated.get('JWT_SECRET')).toBeTruthy();
+    expect(generated.get('DOMAIN')).toBe('example.com');
+    expect(generated.get('RUNTIPI_APP_DATA_PATH')).toBe(envMap.get('ROOT_FOLDER_HOST'));
+    expect(generated.get('POSTGRES_HOST')).toBe('runtipi-db');
+    expect(generated.get('POSTGRES_DBNAME')).toBe('tipi');
+    expect(generated.get('POSTGRES_USERNAME')).toBe('tipi');
+    expect(generated.get('POSTGRES_PORT')).toBe('5432');
+    expect(generated.get('REDIS_HOST')).toBe('runtipi-redis');
+    expect(generated.get('DEMO_MODE')).toBe('false');
+    expect(generated.get('GUEST_DASHBOARD')).toBe('false');
+    expect(generated.get('LOCAL_DOMAIN')).toBe('tipi.lan');
+    expect(generated.get('ALLOW_AUTO_THEMES')).toBe('true');
+    expect(generated.get('ALLOW_ERROR_MONITORING')).toBe('false');
+    expect(generated.get('PERSIST_TRAEFIK_CONFIG')).toBe('false');
+    expect(generated.get('GIT_VERIFY_SSL')).toBe('true');
   });
 
-  it("should replace any old repo url from settings.json to the new one", async () => {
+  it('should replace any old repo url from settings.json to the new one', async () => {
     // arrange
     prepareEnvFile();
     await fs.promises.writeFile(
-      path.join(DATA_DIR, "state", "settings.json"),
+      path.join(DATA_DIR, 'state', 'settings.json'),
       JSON.stringify({
-        appsRepoUrl: "https://github.com/meienberger/runtipi-appstore",
-      })
+        appsRepoUrl: 'https://github.com/meienberger/runtipi-appstore',
+      }),
     );
 
     // act
     const generated = await generateSystemEnvFile();
 
     // assert
-    expect(generated.get("APPS_REPO_URL")).toBe(
-      "https://github.com/runtipi/runtipi-appstore"
-    );
+    expect(generated.get('APPS_REPO_URL')).toBe('https://github.com/runtipi/runtipi-appstore');
   });
 
-  it("should not replace a repo url from settings.json if it is not the old one", async () => {
+  it('should not replace a repo url from settings.json if it is not the old one', async () => {
     // arrange
     prepareEnvFile();
     const url = faker.internet.url();
     await fs.promises.writeFile(
-      path.join(DATA_DIR, "state", "settings.json"),
+      path.join(DATA_DIR, 'state', 'settings.json'),
       JSON.stringify({
         appsRepoUrl: url,
-      })
+      }),
     );
 
     // act
     const generated = await generateSystemEnvFile();
 
     // assert
-    expect(generated.get("APPS_REPO_URL")).toBe(url);
+    expect(generated.get('APPS_REPO_URL')).toBe(url);
   });
 
-  it("should not replace an existing JWT_SECRET from env file", async () => {
+  it('should not replace an existing JWT_SECRET from env file', async () => {
     // arrange
     const secret = faker.word.adverb();
-    envMap.set("JWT_SECRET", secret);
+    envMap.set('JWT_SECRET', secret);
     prepareEnvFile();
 
     // act
     const generated = await generateSystemEnvFile();
 
     // assert
-    expect(generated.get("JWT_SECRET")).toBe(secret);
+    expect(generated.get('JWT_SECRET')).toBe(secret);
   });
 });
