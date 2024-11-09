fe8b1922192e530d3c66d9435b3fdd912d7c5572
Nicolas Meienberger <github@thisprops.com>
2024-11-09 17:19:23 +0100

    ci: fix playwright cache

--- e2e/0005-guest-dashboard.spec.ts
+++ e2e/0005-guest-dashboard.spec.ts
@@ -5,7 +5,7 @@ import { clearDatabase, db } from './helpers/db';
 import { setSettings } from './helpers/settings';
 
 test.beforeEach(async () => {
-  test.fixme(true, 'Not working yet');
+  // test.fixme(true, 'Not working yet');
   await clearDatabase();
   await setSettings({});
 });
@@ -18,8 +18,6 @@ test('user can activate the guest dashboard and see it when logged out', async (
   await page.getByLabel('guestDashboard').setChecked(true);
   await page.getByRole('button', { name: 'Update settings' }).click();
   await page.getByTestId('logout-button').click();
-
-  await page.reload();
 
   await expect(page.getByText('No apps to display')).toBeVisible();
 });
@@ -56,8 +54,6 @@ test('logged out users can see the apps on the guest dashboard', async ({ browse
   await page.getByRole('button', { name: 'Update settings' }).click();
   await page.getByTestId('logout-button').click();
 
-  await page.reload();
-
   await expect(page.getByText(/Hello World web server/)).toBeVisible();
   const locator = page.locator('text=Actual Budget');
   await expect(locator).not.toBeVisible();
@@ -81,8 +77,6 @@ test('user can deactivate the guest dashboard and not see it when logged out', a
   await page.getByLabel('guestDashboard').setChecked(false);
   await page.getByRole('button', { name: 'Update settings' }).click();
   await page.getByTestId('logout-button').click();
-
-  await page.reload();
 
   // We should be redirected to the login page
   await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
