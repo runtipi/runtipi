fdac535cfc769f1815b8bafae559ccc3b6f6ef1d
Stavros <steveiliop56@gmail.com>
2024-11-19 18:59:52 +0200

    fix: do not append EOL

--- packages/backend/src/core/filesystem/filesystem.service.ts
+++ packages/backend/src/core/filesystem/filesystem.service.ts
@@ -49,8 +49,7 @@ export class FilesystemService {
 
   async writeJsonFile<T>(filePath: string, data: T): Promise<boolean> {
     try {
-      await fs.promises.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
-      await fs.promises.appendFile(filePath, EOL, 'utf8');
+      await fs.promises.writeFile(filePath, `${JSON.stringify(data, null, 2)}${EOL}`, 'utf8');
       return true;
     } catch (error) {
       this.logger.error(`Error writing file ${filePath}: ${error}`);
@@ -61,8 +60,7 @@ export class FilesystemService {
   async writeTextFile(filePath: string, content: string): Promise<boolean> {
     try {
       await fs.promises.mkdir(filePath.split('/').slice(0, -1).join('/'), { recursive: true });
-      await fs.promises.writeFile(filePath, content, 'utf8');
-      await fs.promises.appendFile(filePath, EOL, 'utf8');
+      await fs.promises.writeFile(filePath, `${content}${EOL}`, 'utf8');
       return true;
     } catch (error) {
       this.logger.error(`Error writing file ${filePath}: ${error}`);
