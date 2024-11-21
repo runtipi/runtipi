c81caaa9229ce780f9d4a3c627feb5d2d263d275
Nicolas Meienberger <github@thisprops.com>
2024-11-21 22:44:46 +0100

    fix(errors): send intl params if server throws translatable error

--- packages/backend/src/common/error/exception.filter.ts
+++ packages/backend/src/common/error/exception.filter.ts
@@ -27,8 +27,22 @@ export class MainExceptionFilter implements ExceptionFilter {
       this.logger.error('Schema validation failed: ', request.path, JSON.stringify(error.errors, null, 2));
     }
 
-    if ((exception instanceof Error && status !== HttpStatus.INTERNAL_SERVER_ERROR) || exception instanceof TranslatableError) {
+    if (exception instanceof Error && status !== HttpStatus.INTERNAL_SERVER_ERROR) {
       message = exception.message;
+    }
+
+    let intlParams: Record<string, string | undefined> | undefined;
+    if (exception instanceof TranslatableError) {
+      const response = exception.getResponse();
+
+      if (typeof response === 'string') {
+        message = response;
+      } else {
+        // @ts-expect-error
+        message = response.message;
+        // @ts-expect-error
+        intlParams = response.intlParams;
+      }
     }
 
     if (status >= 500 && !(exception instanceof TranslatableError)) {
@@ -44,6 +58,7 @@ export class MainExceptionFilter implements ExceptionFilter {
       statusCode: status,
       message,
       path: request.url,
+      intlParams,
     });
   }
 }
