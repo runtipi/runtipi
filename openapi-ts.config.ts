42749de7ffc9f021e30e73ce8cfeab611ba7316e
Nicolas Meienberger <github@thisprops.com>
2024-11-27 20:57:57 +0100

    chore(frontend): generate sdk

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from '@hey-api/openapi-ts';
 export default defineConfig({
   client: '@hey-api/client-fetch',
   input: './packages/backend/src/swagger.json',
-  types: {
-    export: true,
-  },
   output: {
     path: './packages/frontend/src/api-client',
     format: 'biome',
