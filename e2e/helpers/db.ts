9ca677b7e1cdf706297a92737db2f39ad1a06b0f
Nicolas Meienberger <github@thisprops.com>
2024-12-08 14:52:29 +0100

    refactor: remove pg pool

--- e2e/helpers/db.ts
+++ e2e/helpers/db.ts
@@ -1,14 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
 import * as schema from '../../packages/backend/src/core/database/drizzle/schema';
 
 const connectionString = `postgresql://tipi:${process.env.POSTGRES_PASSWORD}@${process.env.SERVER_IP}:5432/tipi?connect_timeout=300`;
 
-const pool = new Pool({
-  connectionString,
-});
-
-export const db = drizzle(pool, { schema });
+export const db = drizzle(connectionString, { schema });
 
 export const clearDatabase = async () => {
   // delete all data in table user
