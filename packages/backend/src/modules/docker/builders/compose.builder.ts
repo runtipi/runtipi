5fc49d96348d4c67f0ba8a067cd41e69aab1d3c7
Nicolas Meienberger <github@thisprops.com>
2025-01-12 13:27:56 +0100

    chore: merge master

--- packages/backend/src/modules/docker/builders/compose.builder.ts
+++ packages/backend/src/modules/docker/builders/compose.builder.ts
@@ -85,28 +85,26 @@ export class DockerComposeBuilder {
       .setStdinOpen(params.stdinOpen);
 
     if (params.isMain) {
-      if (!params.internalPort) {
-        throw new Error('Main service must have an internal port specified');
-      }
-
-      if (form.openPort) {
+      if (form.openPort && params.internalPort) {
         service.setPort({
           containerPort: params.internalPort,
           hostPort: '${APP_PORT}',
         });
       }
 
-      const traefikLabels = new TraefikLabelsBuilder({
-        storeId,
-        internalPort: params.internalPort,
-        appId: params.name,
-        exposedLocal: form.exposedLocal,
-        exposed: form.exposed,
-      })
-        .addExposedLabels()
-        .addExposedLocalLabels();
+      if (params.internalPort) {
+        const traefikLabels = new TraefikLabelsBuilder({
+          storeId,
+          internalPort: params.internalPort,
+          appId: params.name,
+          exposedLocal: form.exposedLocal,
+          exposed: form.exposed,
+        })
+          .addExposedLabels()
+          .addExposedLocalLabels();
 
-      service.setLabels(traefikLabels.build());
+        service.setLabels(traefikLabels.build());
+      }
     }
 
     return service.build();
