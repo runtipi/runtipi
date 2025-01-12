fb5b087971995cd269b93e3e5f958ddb584a3ca5
Stavros <steveiliop56@gmail.com>
2025-01-12 15:19:18 +0200

    feat: translations

--- packages/frontend/src/modules/settings/components/advanced-settings-modal/advanced-settings-modal.tsx
+++ packages/frontend/src/modules/settings/components/advanced-settings-modal/advanced-settings-modal.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/Button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/Dialog";;
 import type { useDisclosure } from "@/lib/hooks/use-disclosure";
+import { useTranslation } from "react-i18next";
 
 interface IProps {
     onEnable: () => void;
@@ -9,23 +10,24 @@ interface IProps {
 
 export const AdvancedSettingsModal = (props: IProps) => {
     const { advancedSettingsDisclosure, onEnable } = props;
+    const { t } = useTranslation(); 
 
     return (
         <div>
             <Dialog open={advancedSettingsDisclosure.isOpen} onOpenChange={advancedSettingsDisclosure.toggle}>
                 <DialogContent size="sm" type="warning">
                     <DialogHeader>
-                        <DialogTitle>Enable advanced settings?</DialogTitle>
+                        <DialogTitle>{t('SETTINGS_GENERAL_ADVANCED_SETTINGS_MODAL_TITLE')}</DialogTitle>
                     </DialogHeader>
                     <DialogDescription>
-                        <span className="text-muted">Advanced settings are intended for experienced users only. Are you sure you want to enable them? Misconfiguring these settings can result in a broken install.</span>
+                        <span className="text-muted">{t('SETTINGS_GENERAL_ADVANCED_SETTINGS_MODAL_SUBTITLE')}</span>
                     </DialogDescription>
                     <DialogFooter>
                         <Button onClick={() => advancedSettingsDisclosure.close()}>
-                            Cancel
+                            {t('ACTIONS_CANCEL')}
                         </Button>
                         <Button intent="warning" onClick={onEnable}>
-                            Enable
+                            {t('ACTIONS_ENABLE')}
                         </Button>
                     </DialogFooter>
                 </DialogContent>
