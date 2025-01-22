400152c77f37a97d1869df20fb1e0de50006eef5
Nicolas Meienberger <github@thisprops.com>
2025-01-22 21:23:50 +0100

    refactor(queue): improve RPC timeouts event handling

--- packages/backend/src/modules/app-lifecycle/commands/install-app-command.ts
+++ packages/backend/src/modules/app-lifecycle/commands/install-app-command.ts
@@ -42,6 +42,12 @@ export class InstallAppCommand extends AppLifecycleCommand {
 
       await this.ensureAppDir(appId, form);
 
+      try {
+        await this.dockerService.composeApp(appUrn, 'down --rmi all --remove-orphans');
+      } catch (err) {
+        this.logger.warn(`No prior containers to remove for app ${appUrn}`);
+      }
+
       // run docker-compose up
       await this.dockerService.composeApp(appId, 'up --detach --force-recreate --remove-orphans --pull always');
 
