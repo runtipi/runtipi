e43c5e75e7de06f485e3b6e8bc30cd708b324fcf
Nicolas Meienberger <github@thisprops.com>
2025-02-09 14:30:24 +0100

    fix: put all existing app entities to enable_auth = false

--- packages/backend/src/core/database/drizzle/0014_curly_jigsaw.sql
+++ packages/backend/src/core/database/drizzle/0014_curly_jigsaw.sql
@@ -1,1 +1,16 @@
-ALTER TABLE "app" ADD COLUMN "enable_auth" boolean DEFAULT false NOT NULL;
+ALTER TABLE "app"
+ADD COLUMN IF NOT EXISTS "enable_auth" boolean DEFAULT false;
+
+-- Select all apps that have not the enable_auth field and put it to false
+UPDATE "app"
+SET
+  "enable_auth" = FALSE
+WHERE
+  "enable_auth" IS NULL;
+
+-- Set enable_auth column to not null constraint
+ALTER TABLE "app"
+ALTER COLUMN "enable_auth"
+SET
+  NOT NULL;
+
