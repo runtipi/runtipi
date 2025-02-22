2877c6023da1b11ead9c7a6e48d29ede6b4bc4e4
Stavros <steveiliop56@gmail.com>
2025-02-23 00:10:25 +0200

    feat: category select clear

--- packages/frontend/src/components/category-selector/category-selector.tsx
+++ packages/frontend/src/components/category-selector/category-selector.tsx
@@ -3,6 +3,7 @@ import { iconForCategory } from '@/modules/app/helpers/table-helpers';
 import type { AppCategory } from '@/types/app.types';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { IconX } from '@tabler/icons-react';
 
 interface Props {
   onSelect: (value?: AppCategory) => void;
@@ -12,6 +13,8 @@ interface Props {
 
 export const CategorySelector = ({ onSelect, className, initialValue }: Props) => {
   const { t } = useTranslation();
+  const [resetCounter, setResetCounter] = useState(0);
+
   const options = iconForCategory.map((category) => ({
     value: category.id,
     label: t(`APP_CATEGORY_${category.id.toUpperCase()}`),
@@ -20,17 +23,40 @@ export const CategorySelector = ({ onSelect, className, initialValue }: Props) =
 
   const [value, setValue] = useState(initialValue);
 
-  const handleChange = (option: AppCategory) => {
+  type Selection = AppCategory | 'clear';
+
+  const handleChange = (option?: Selection) => {
+    if (option === 'clear') {
+      handleReset();
+      return;
+    }
     setValue(option);
     onSelect(option);
   };
 
+  const handleReset = () => {
+    setValue(undefined);
+    onSelect(undefined);
+    setResetCounter((prev) => prev + 1);
+  };
+
   return (
-    <Select value={value} onValueChange={handleChange}>
+    <Select key={resetCounter.toString()} value={value} onValueChange={(o: AppCategory) => handleChange(o)}>
       <SelectTrigger value={value} className={className}>
-        <SelectValue placeholder={t('APP_INSTALL_FORM_CHOOSE_OPTION')} />
+        <SelectValue placeholder={t('APP_STORE_CHOOSE_CATEGORY')} />
       </SelectTrigger>
       <SelectContent>
+        {value && (
+          <>
+            <SelectItem key="clear" value="clear">
+              <span className="d-flex gap-2">
+                <IconX size={20} />
+                {t('CLEAR')}
+              </span>
+            </SelectItem>
+            <div className="dropdown-divider" />
+          </>
+        )}
         {options?.map(({ value: category, icon: CategoryIcon, label }) => (
           <SelectItem key={category} value={category}>
             <span className="d-flex gap-2">
