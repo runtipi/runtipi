363c5902d9631aca7be5dc79cf95a91f617cec8a
Nicolas Meienberger <github@thisprops.com>
2025-02-22 23:07:14 +0100

    Revert "feat: diselect selection"
    This reverts commit e4f4cf7fbdbb9c9f36415f49573594878a0e8a34.

--- packages/frontend/src/components/category-selector/category-selector.tsx
+++ packages/frontend/src/components/category-selector/category-selector.tsx
@@ -12,8 +12,6 @@ interface Props {
 
 export const CategorySelector = ({ onSelect, className, initialValue }: Props) => {
   const { t } = useTranslation();
-  const [resetCounter, setResetCounter] = useState(0);
-
   const options = iconForCategory.map((category) => ({
     value: category.id,
     label: t(`APP_CATEGORY_${category.id.toUpperCase()}`),
@@ -22,21 +20,15 @@ export const CategorySelector = ({ onSelect, className, initialValue }: Props) =
 
   const [value, setValue] = useState(initialValue);
 
-  const handleChange = (option?: AppCategory) => {
+  const handleChange = (option: AppCategory) => {
     setValue(option);
     onSelect(option);
   };
 
-  const handleReset = () => {
-    setValue(undefined);
-    onSelect(undefined);
-    setResetCounter((prev) => prev + 1);
-  };
-
   return (
-    <Select key={resetCounter.toString()} value={value} onValueChange={(o: AppCategory) => handleChange(o)}>
-      <SelectTrigger value={value} onClear={handleReset} className={className}>
-        <SelectValue placeholder={t('APP_STORE_CHOOSE_CATEGORY')} />
+    <Select value={value} onValueChange={handleChange}>
+      <SelectTrigger value={value} className={className}>
+        <SelectValue placeholder={t('APP_INSTALL_FORM_CHOOSE_OPTION')} />
       </SelectTrigger>
       <SelectContent>
         {options?.map(({ value: category, icon: CategoryIcon, label }) => (
