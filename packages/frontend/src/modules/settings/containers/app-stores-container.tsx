5a0a460414c26d7a1cc86c0c8020ec36fc93b58f
Stavros <steveiliop56@gmail.com>
2025-04-18 13:11:25 +0300

    fix: use tabler's new styles for alerts

--- packages/frontend/src/modules/settings/containers/app-stores-container.tsx
+++ packages/frontend/src/modules/settings/containers/app-stores-container.tsx
@@ -1,6 +1,6 @@
 import { getAllAppStoresOptions } from '@/api-client/@tanstack/react-query.gen';
-import { Alert, AlertSubtitle, AlertTitle } from '@/components/ui/Alert/Alert';
-import { IconAlertTriangle, IconBrandAppstore } from '@tabler/icons-react';
+import { Alert, AlertDescription, AlertHeading, AlertIcon } from '@/components/ui/Alert/Alert';
+import { IconAlertCircle, IconBrandAppstore } from '@tabler/icons-react';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { AppStoresTable } from '../components/app-stores-table/app-stores-table';
@@ -20,14 +20,12 @@ export const AppStoresContainer = () => {
       </div>
       <p className="text-muted">{t('SETTINGS_APPSTORES_SUBTITLE')}</p>
       <Alert variant="warning">
-        <div className="d-flex align-items-center">
-          <div className="me-3">
-            <IconAlertTriangle stroke={2} />
-          </div>
-          <div>
-            <AlertTitle>{t('COMMON_WARNING')}</AlertTitle>
-            <AlertSubtitle>{t('SETTINGS_APPSTORES_WARNING')}</AlertSubtitle>
-          </div>
+        <AlertIcon>
+          <IconAlertCircle stroke={2} />
+        </AlertIcon>
+        <div>
+          <AlertHeading>{t('COMMON_WARNING')}</AlertHeading>
+          <AlertDescription>{t('SETTINGS_APPSTORES_WARNING')}</AlertDescription>
         </div>
       </Alert>
       <AppStoresTable appStores={data.appStores} />
