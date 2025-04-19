db0931aebe0d5a08d4ac558b321a125f6b5d318e
Nicolas Meienberger <github@thisprops.com>
2025-04-19 10:43:13 +0200

    fix: light mode

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
   },
   server: {
