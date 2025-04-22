33535f593f67a16a1b47a655aaf01816f47117a3
Nicolas Meienberger <github@thisprops.com>
2025-04-22 08:15:53 +0200

    RELEASING: Releasing 3 package(s)
    Releases:
      @runtipi/common@0.6.0
      backend@0.0.5
      frontend@0.0.4
    
    [skip ci]

--- packages/common/src/schemas/index.ts
+++ packages/common/src/schemas/index.ts
@@ -1,4 +1,6 @@
 import { dynamicComposeSchema, serviceSchema } from './dynamic-compose.js';
 import type { DependsOn, Service, ServiceInput } from './dynamic-compose.js';
+import { toJsonSchema } from './utils/to-json-schema.js';
 
-export { dynamicComposeSchema, serviceSchema, type ServiceInput, type DependsOn, type Service };
+export { dynamicComposeSchema, serviceSchema, type ServiceInput, type DependsOn, type Service, toJsonSchema };
+
--- packages/frontend/CHANGELOG.md
+++ packages/frontend/CHANGELOG.md
@@ -1,5 +1,15 @@
 # frontend
 
+## 0.0.4
+
+### Patch Changes
+
+- Updated dependencies
+- Updated dependencies
+- Updated dependencies
+  - @runtipi/common@0.6.0
+  - backend@0.0.5
+
 ## 0.0.3
 
 ### Patch Changes
