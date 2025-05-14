1f24eea77de6660eaff9534eb11cc7e1d1a38905
Nicolas Meienberger <github@thisprops.com>
2025-05-14 08:48:39 +0200

    chore: bump swc/cli to v0.7.7

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
   exit
 fi
 
 echo "Nuking the system..."
 
 # Remove all runtipi data
-
-rm -rf app-data
-rm -rf apps
-rm -rf backups
-rm -rf cache
-rm -rf logs
-rm -rf media
-rm -rf repos
-rm -rf state
-rm -rf traefik
-rm -rf user-config
+rm -rf .internal
 
 # Remove containers
-
-docker rm -f runtipi runtipi-reverse-proxy runtipi-db
+docker rm -f runtipi runtipi-reverse-proxy runtipi-db runtipi-queue
 
 # Remove docker volumes
-
 docker volume rm runtipi_pgdata
