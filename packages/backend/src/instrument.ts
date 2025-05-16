82c15a10e590a07f607b81690053853f767925f3
Nicolas Meienberger <github@thisprops.com>
2025-05-16 18:43:34 +0200

    chore: enable sentry profiling

--- packages/backend/src/instrument.ts
+++ packages/backend/src/instrument.ts
@@ -1,15 +1,18 @@
 import * as Sentry from '@sentry/nestjs';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { cleanseErrorData } from './common/helpers/error-helpers';
 
 Sentry.init({
   release: process.env.TIPI_VERSION,
   enabled: false,
   tracesSampleRate: 1.0,
+  profileSessionSampleRate: 1.0,
+  profileLifecycle: 'trace',
   dsn: 'https://6cc88df40d1cdd0222ff30d996ca457c@o4504242900238336.ingest.us.sentry.io/4508264534835200',
   environment: process.env.NODE_ENV,
   beforeSend: cleanseErrorData,
   includeLocalVariables: true,
-  integrations: [Sentry.extraErrorDataIntegration(), Sentry.nestIntegration()],
+  integrations: [Sentry.extraErrorDataIntegration(), Sentry.nestIntegration(), nodeProfilingIntegration()],
   initialScope: {
     tags: { version: process.env.TIPI_VERSION },
   },
