543b8541d0ced688f46b58141124ee49e37e496b
Nicolas Meienberger <github@thisprops.com>
2025-10-07 19:14:54 +0200

    chore: update api client

--- packages/frontend/src/api-client/client/client.gen.ts
+++ packages/frontend/src/api-client/client/client.gen.ts
@@ -86,7 +86,7 @@ export const createClient = (config: Config = {}): Client => {
 
     let request = new Request(url, requestInit);
 
-    for (const fn of interceptors.request._fns) {
+    for (const fn of interceptors.request.fns) {
       if (fn) {
         request = await fn(request, opts);
       }
@@ -97,7 +97,7 @@ export const createClient = (config: Config = {}): Client => {
     const _fetch = opts.fetch!;
     let response = await _fetch(request);
 
-    for (const fn of interceptors.response._fns) {
+    for (const fn of interceptors.response.fns) {
       if (fn) {
         response = await fn(response, request, opts);
       }
@@ -192,7 +192,7 @@ export const createClient = (config: Config = {}): Client => {
     const error = jsonError ?? textError;
     let finalError = error;
 
-    for (const fn of interceptors.error._fns) {
+    for (const fn of interceptors.error.fns) {
       if (fn) {
         finalError = (await fn(error, response, request, opts)) as string;
       }
@@ -227,7 +227,7 @@ export const createClient = (config: Config = {}): Client => {
         method,
         onRequest: async (url, init) => {
           let request = new Request(url, init);
-          for (const fn of interceptors.request._fns) {
+          for (const fn of interceptors.request.fns) {
             if (fn) {
               request = await fn(request, opts);
             }
--- packages/frontend/src/api-client/client/utils.gen.ts
+++ packages/frontend/src/api-client/client/utils.gen.ts
@@ -244,67 +244,61 @@ type ResInterceptor<Res, Req, Options> = (
 ) => Res | Promise<Res>;
 
 class Interceptors<Interceptor> {
-  _fns: (Interceptor | null)[];
+  fns: Array<Interceptor | null> = [];
 
-  constructor() {
-    this._fns = [];
+  clear(): void {
+    this.fns = [];
   }
 
-  clear() {
-    this._fns = [];
+  eject(id: number | Interceptor): void {
+    const index = this.getInterceptorIndex(id);
+    if (this.fns[index]) {
+      this.fns[index] = null;
+    }
+  }
+
+  exists(id: number | Interceptor): boolean {
+    const index = this.getInterceptorIndex(id);
+    return Boolean(this.fns[index]);
   }
 
   getInterceptorIndex(id: number | Interceptor): number {
     if (typeof id === 'number') {
-      return this._fns[id] ? id : -1;
-    } else {
-      return this._fns.indexOf(id);
+      return this.fns[id] ? id : -1;
     }
-  }
-  exists(id: number | Interceptor) {
-    const index = this.getInterceptorIndex(id);
-    return !!this._fns[index];
+    return this.fns.indexOf(id);
   }
 
-  eject(id: number | Interceptor) {
+  update(
+    id: number | Interceptor,
+    fn: Interceptor,
+  ): number | Interceptor | false {
     const index = this.getInterceptorIndex(id);
-    if (this._fns[index]) {
-      this._fns[index] = null;
-    }
-  }
-
-  update(id: number | Interceptor, fn: Interceptor) {
-    const index = this.getInterceptorIndex(id);
-    if (this._fns[index]) {
-      this._fns[index] = fn;
+    if (this.fns[index]) {
+      this.fns[index] = fn;
       return id;
-    } else {
-      return false;
     }
+    return false;
   }
 
-  use(fn: Interceptor) {
-    this._fns = [...this._fns, fn];
-    return this._fns.length - 1;
+  use(fn: Interceptor): number {
+    this.fns.push(fn);
+    return this.fns.length - 1;
   }
 }
 
-// `createInterceptors()` response, meant for external use as it does not
-// expose internals
 export interface Middleware<Req, Res, Err, Options> {
-  error: Pick<
-    Interceptors<ErrInterceptor<Err, Res, Req, Options>>,
-    'eject' | 'use'
-  >;
-  request: Pick<Interceptors<ReqInterceptor<Req, Options>>, 'eject' | 'use'>;
-  response: Pick<
-    Interceptors<ResInterceptor<Res, Req, Options>>,
-    'eject' | 'use'
-  >;
+  error: Interceptors<ErrInterceptor<Err, Res, Req, Options>>;
+  request: Interceptors<ReqInterceptor<Req, Options>>;
+  response: Interceptors<ResInterceptor<Res, Req, Options>>;
 }
 
-// do not add `Middleware` as return type so we can use _fns internally
-export const createInterceptors = <Req, Res, Err, Options>() => ({
+export const createInterceptors = <Req, Res, Err, Options>(): Middleware<
+  Req,
+  Res,
+  Err,
+  Options
+> => ({
   error: new Interceptors<ErrInterceptor<Err, Res, Req, Options>>(),
   request: new Interceptors<ReqInterceptor<Req, Options>>(),
   response: new Interceptors<ResInterceptor<Res, Req, Options>>(),
