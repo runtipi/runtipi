216f8d36e21b60fa4d6cf3ea18cf2789faa6228a
Stavros <steveiliop56@gmail.com>
2025-10-16 17:55:57 +0300

    refactor: use hover animation in button tile

--- packages/frontend/src/modules/app/components/button-tile/button-tile.tsx
+++ packages/frontend/src/modules/app/components/button-tile/button-tile.tsx
@@ -10,13 +10,13 @@ interface ButtonTileProps {
 export const ButtonTile: React.FC<ButtonTileProps> = ({ title, subtitle, icon, action }: ButtonTileProps) => {
   return (
     <button onClick={action} className="col-sm-6 col-lg-4 button-tile p-2 pt-0 pb-0 mb-0" type="button">
-      <div className="card card-sm card-link ml-0">
+      <div className="card card-sm ml-0 link-primary link-primary-subtle">
         <div className="card-body d-flex align-items-center gap-3">
           <div className="d-flex align-items-center justify-content-center" style={{ width: '60px', height: '60px' }}>
             {icon}
           </div>
           <div>
-            <div className="fw-bolder text-start">{title}</div>
+            <div className="fw-bolder text-start ">{title}</div>
             <div className="text-muted text-start">{subtitle}</div>
           </div>
         </div>
