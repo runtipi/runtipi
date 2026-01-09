128553dfe32dd66bba459c75cd7a279b2a79c8fd
Nicolas Meienberger <github@thisprops.com>
2026-01-09 17:26:05 +0100

    fix(logs): esacape XML tags closes #2535

--- packages/backend/src/modules/docker/helpers/colorize-logs.ts
+++ packages/backend/src/modules/docker/helpers/colorize-logs.ts
@@ -1,14 +1,16 @@
 import Convert from 'ansi-to-html';
 
-const convert = new Convert();
+const convert = new Convert({
+  escapeXML: true,
+});
 
 export const colorizeLogs = async (lines: string[]) =>
   await Promise.all(
     lines.map(async (line: string) => {
       try {
         return convert.toHtml(line);
-      } catch (e) {
-        return line;
+      } catch (_e) {
+        return line.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
       }
     }),
   );
