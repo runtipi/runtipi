8ab3fe8655c0eaed36466a2b16aa3aa639d061dd
Stavros <steveiliop56@gmail.com>
2026-07-08 19:10:50 +0300

    refactor: use new api endpoints in frontend

--- packages/frontend/src/types/oidc.types.ts
+++
@@ -1,5 +0,0 @@
-import type { GetProvidersPrivateResponse } from '@/api-client';
-
-export interface Provider extends Omit<GetProvidersPrivateResponse['providers'][number], 'id'> {
-  id: number;
-}
